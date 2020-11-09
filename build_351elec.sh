12ece46c6a11360b14db88a28c894b3215637c69
Fewtarius <fewtarius@gmail.com>
2020-11-09 16:25:14 -0500

    Improve the build script a bit

--- build_351elec.sh
+++ build_351elec.sh
@@ -9,8 +9,7 @@ export PROJECT=Rockchip
 export DEVICE=RG351P
 export ARCH=arm
 
-
-if [ ! "${1^^}" == "WORLD" ]
+if [ ! "${1^^}" == "WORLD" ] && [ ! "${1^^}" == "RESET" ]
 then
   BUILD=$(echo ${BUILD} | sed 's#^.*-##')
   BUILD="${DEVICE}-${1^^}"
@@ -19,7 +18,6 @@ else
 fi
 
 function additional_packages () {
-  git checkout projects/Rockchip/options
   if [ "$1" == "emulators" ]
   then
     sed -i 's/@ADDITIONAL_PACKAGES@/dtc emuelec dosbox-sdl2 fba4arm mupen64plus-nx PPSSPPSDL amiberry advancemame hatarisa hatari/g' projects/Rockchip/options
@@ -30,16 +28,17 @@ function additional_packages () {
 
 function RG351P-LI () {
   echo "Checking out the latest Linux kernel packages for building."
-  git checkout packages/linux
-  git checkout packages/linux-driver-addons
-  git checkout packages/linux-drivers
-  git checkout packages/linux-firmware
-  git checkout packages/virtual/linux-drivers
-  git checkout packages/virtual/linux-firmware
   echo "Setting build device directory to ${BUILD}"
   rm -rf projects/Rockchip/devices/RG351P
   cp -rf projects/Rockchip/devices/RG351P-LI projects/Rockchip/devices/RG351P
-  make image
+  make $1
+  if [ -d "release/RG351P-LI" ]
+  then
+    rm -rf "release/RG351P-LI"
+  fi
+  mv target release/RG351P-LI
+  mkdir target
+  RESET
 }
 
 function RG351P-AK () {
@@ -52,23 +51,55 @@ function RG351P-AK () {
   rm -rf packages/virtual/linux-firmware
   rm -rf projects/Rockchip/devices/RG351P
   cp -rf projects/Rockchip/devices/RG351P-AK projects/Rockchip/devices/RG351P
+  make $1
+  if [ -d "release/RG351P-AK" ]
+  then
+    rm -rf "release/RG351P-AK"
+  fi
+  mv target release/RG351P-AK
+  mkdir target
+  RESET
 }
+
+function RESET () {
+  git checkout projects/Rockchip/options
+  git checkout packages/linux
+  git checkout packages/linux-driver-addons
+  git checkout packages/linux-drivers
+  git checkout packages/linux-firmware
+  git checkout packages/virtual/linux-drivers
+  git checkout packages/virtual/linux-firmware
+}
+
+for dir in release target
+do
+  if [ ! -d "${dir}" ]
+  then
+    mkdir ${dir}
+  fi
+done
+
+RESET
 
 if [ "${BUILD}" == "RG351P-LI" ]
 then
-  additional_packages
-  RG351P-LI
+  additional_packages emulators
+  RG351P-LI image
 elif [ "${BUILD}" == "RG351P-AK" ]
 then
-  additional_packages
-  RG351P-AK
+  additional_packages emulators
+  RG351P-AK image
 elif [ "${BUILD}" == "WORLD" ]
 then
   additional_packages
-  RG351P-LI
+  RG351P-LI image
   additional_packages emulators
-  RG351P-LI
-  RG351P-AK
+  RG351P-LI image
+  additional_packages emulators
+  RG351P-AK image
+elif [ "${BUILD}" == "RESET" ]
+then
+  RESET
 else
   echo "Pass the project to build (LI,AL,WORLD) as an argument on the command line. [${BUILD}]"
   exit 1
