5b6ada2a56fb3b206b5b27e8d5d61389d3252a28
Fewtarius <fewtarius@gmail.com>
2020-11-15 16:13:28 -0500

    Fix missing mupen cores

--- packages/sx05re/emulators/mupen64plus-nx/package.mk
+++ packages/sx05re/emulators/mupen64plus-nx/package.mk
@@ -9,7 +9,7 @@ PKG_VERSION="b785150465048fa88f812e23462f318e66af0be0"
 PKG_SHA256="456c433f45b0e2ba15a587978234e3e1300301d431b6823747ad0e779331c97e"
 fi
 PKG_REV="1"
-PKG_ARCH="arm"
+PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro-nx"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
@@ -23,19 +23,36 @@ PKG_BUILD_FLAGS="-lto"
 pre_configure_target() {
   sed -e "s|^GIT_VERSION ?.*$|GIT_VERSION := \" ${PKG_VERSION:0:7}\"|" -i Makefile
 
+if [ $ARCH == "arm" ]; then
 if [ ${PROJECT} = "Amlogic-ng" ]; then
 	PKG_MAKE_OPTS_TARGET+=" platform=AMLG12B"
 	sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
 	sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
 elif [ "${PROJECT}" = "Amlogic" ]; then
 	PKG_MAKE_OPTS_TARGET+=" platform=amlogic"
-elif [ "${DEVICE}" = "OdroidGoAdvance" ] || [ "$DEVICE" == "RG351P" ]; then
+elif [ "${DEVICE}" = "RG351P" ]; then
 	sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
 	sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
 	sed -i "s|cortex-a53|cortex-a35|g" Makefile
-	PKG_MAKE_OPTS_TARGET+=" platform=RK3328"
+	PKG_MAKE_OPTS_TARGET+=" platform=odroidgoa"
+	fi
+else
+	if [ ${PROJECT} = "Amlogic-ng" ]; then
+		PKG_MAKE_OPTS_TARGET+=" platform=odroid64 BOARD=N2"
+		sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
+		sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
+	elif [ "${PROJECT}" = "Amlogic" ]; then 
+		sed -i "s|GLES = 1|GLES = 1|g" Makefile
+		sed -i "s|-lGLESv2|-lGLESv2|g" Makefile
+		PKG_MAKE_OPTS_TARGET+=" platform=amlogic64"
+	elif [ "${DEVICE}" = "RG351P" ]; then
+		sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
+		sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
+		PKG_MAKE_OPTS_TARGET+=" platform=amlogic64"
+	fi
 fi
 }
+
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
