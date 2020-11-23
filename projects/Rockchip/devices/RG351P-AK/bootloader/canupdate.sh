23540cb8b13ef566bcfdef6fff2ba88510a1650a
Fewtarius <fewtarius@gmail.com>
2020-11-23 06:35:05 -0500

    Cleanup related to AK deprecation, 1.0.3 release prep.

--- projects/Rockchip/devices/RG351P-AK/bootloader/canupdate.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
-
-# Allow upgrades between arm and aarch64
-if [ "$1" = "@PROJECT@.arm" -o "$1" = "@PROJECT@.aarch64" ]; then
-  exit 0
-else
-  exit 1
-fi
