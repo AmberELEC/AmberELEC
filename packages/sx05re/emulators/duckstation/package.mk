278c6126219377337fb133615c766e555170e10c
Fewtarius <fewtarius@gmail.com>
2020-11-23 21:31:16 -0500

    Add duckstation.

---
+++ packages/sx05re/emulators/duckstation/package.mk
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="duckstation"
+PKG_VERSION="8432050094a485fbdf94ed79fe3eb7764612e9ca"
+PKG_SHA256="6bcd81596a78ef92c5558e038ed3f6c4e9a544c19cead9f887a667ea00d97c45"
+PKG_ARCH="aarch64"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/stenzek/duckstation"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="DuckStation - PlayStation 1, aka. PSX Emulator"
+PKG_TOOLCHAIN="cmake"
+PKG_BUILD_FLAGS="-lto"
+
+pre_configure_target() {
+ PKG_CMAKE_OPTS_TARGET+=" -DCMAKE_BUILD_TYPE=Release -DBUILD_LIBRETRO_CORE=ON "
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/.$TARGET_NAME/duckstation_libretro.so $INSTALL/usr/lib/libretro/
+}
