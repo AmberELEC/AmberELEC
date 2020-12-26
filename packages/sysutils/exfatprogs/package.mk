5dc9a931d6a219f5723e7e943365a2b4afb4dad6
Fewtarius <fewtarius@gmail.com>
2020-12-26 12:52:52 -0500

    Add exfatprogs and update a few kernel configuration items found to be needed in testing

---
+++ packages/sysutils/exfatprogs/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="exfatprogs"
+PKG_VERSION="7c3b61769bb04eaafedf245db396813bbcfb2b0a"
+#PKG_SHA256="1f357e7026407cba6617acd2a0109ede8292e5ede4addd918c65a7b700ef96cb"
+PKG_LICENSE="GPLv2+"
+PKG_SITE="https://github.com/exfatprogs/exfatprogs"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_LONGDESC="Userspace utilities for exfat"
+PKG_TOOLCHAIN="autotools"
