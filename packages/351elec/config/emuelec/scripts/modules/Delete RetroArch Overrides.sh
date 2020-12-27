19aa30fa77f5b08e881349252d3a8b0323d80b96
Fewtarius <fewtarius@gmail.com>
2020-12-27 11:46:40 -0500

    * Remove RetroArch override removal script. * Add Install Half Life to Ports.

--- packages/351elec/config/emuelec/scripts/modules/Delete RetroArch Overrides.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. /etc/profile
-
-message_stream "Deleting all RetroArch Overrides" .02
-rm -rf /storage/.config/retroarch/config/*
---
+++ packages/351elec/config/emuelec/scripts/modules/Install Half Life.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Fewtarius
+
+source /emuelec/scripts/env.sh
+source /etc/profile
+
+INSTALLPATH="/storage/roms/ports"
+PORTNAME="half-life"
+
+if [ ! -d "${INSTALLPATH}/${PORTNAME}" ]
+then
+  mkdir "${INSTALLPATH}/${PORTNAME}"
+fi
+
+clear >/dev/console
+message_stream "Installing Half Life..." .02
+
+cd ${INSTALLPATH}/${PORTNAME}
+for bin in client_arm64.so controls.png hl_arm64.so libxash.so libxashmenu64.so xash3d "Copy Contents into valve folder.zip"
+do
+  curl -Lo ${bin} "https://github.com/krishenriksen/Half-Life-rg351p/raw/main/Half-Life/${bin}"
+done
+
+### Create the start script
+cat <<EOF >${INSTALLPATH}/"Half Life.sh"
+export LD_LIBRARY_PATH=${INSTALLPATH}/${PORTNAME}:/usr/lib
+
+cd ${INSTALLPATH}/${PORTNAME}
+
+if [ ! -d "valve" ]
+then
+  mkdir "valve"
+  echo "Unable to find game data.  Please copy to ${INSTALLPATH}/${PORTNAME}/valve" >>/tmp/logs/emuelec.log
+  exit 1
+else
+  cd valve
+  unzip -o ../"Copy Contents into valve folder.zip"
+  cd ..
+  ./xash3d -fullscreen -console -sdl_joy_old_api
+fi
+
+
+ret_error=$?
+[[ "$ret_error" != 0 ]] && (echo "Error executing Half Life.  Please check that you have copied your game data to ${INSTALLPATH}/${PORTNAME}/valve." >/tmp/logs/emuelec.log)
+EOF
+
+if [ ! "$(grep -q 'Half Life' ${INSTALLPATH}/gamelist.xml)" ]
+then
+	### Add to the game list
+	xmlstarlet ed --omit-decl --inplace \
+		-s '//gameList' -t elem -n 'game' \
+		-s '//gameList/game[last()]' -t elem -n 'path'        -v './Half Life.sh'\
+		-s '//gameList/game[last()]' -t elem -n 'name'        -v 'Half Life'\
+		-s '//gameList/game[last()]' -t elem -n 'desc'        -v 'Half-Life is a series of first-person shooter games developed and published by Valve.'\
+		-s '//gameList/game[last()]' -t elem -n 'image'       -v './images/system-half-life.png'\
+		-s '//gameList/game[last()]' -t elem -n 'video'       -v './images/system-half-life.mp4'\
+		-s '//gameList/game[last()]' -t elem -n 'thumbnail'   -v './images/system-half-life-thumb.png'\
+		-s '//gameList/game[last()]' -t elem -n 'rating'      -v '1.0'\
+		-s '//gameList/game[last()]' -t elem -n 'releasedate' -v '19981119T000000'\
+		-s '//gameList/game[last()]' -t elem -n 'developer'   -v 'Valve'\
+		-s '//gameList/game[last()]' -t elem -n 'publisher'   -v 'Valve'\
+		${INSTALLPATH}/gamelist.xml
+fi
+message_stream "\nRestarting EmulationStation..." .02
