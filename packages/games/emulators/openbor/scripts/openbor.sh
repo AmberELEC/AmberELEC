f82b4bf269c833d5e2e029fbd1ddbd92ddaab6e9
Fewtarius <fewtarius@gmail.com>
2020-12-31 18:10:03 -0500

    * Set OpenBOR to 3:2 and RGA scaling - temporary

--- packages/games/emulators/openbor/scripts/openbor.sh
+++ packages/games/emulators/openbor/scripts/openbor.sh
@@ -12,8 +12,18 @@ fi
 
 ln -sf "${1}" /storage/openbor/Paks/Game.pak
 
+### Maybe we can integrate this better so it uses the ES configurations
+### but for now, a quick fix.
+
+rm -f /storage/.config/retroarch/config/PPSSPP/EBOOT.cfg /storage/.config/retroarch/config/PPSSPP/PPSSPP.opt
+cat <<EOF >/storage/.config/retroarch/config/PPSSPP/EBOOT.cfg
+aspect_ratio_index = "7"
+video_ctx_scaling = "true"
+EOF
+
 # Run retroarch PSP core and start OpenBOR engine
 /usr/bin/retroarch -L /tmp/cores/ppsspp_libretro.so /storage/openbor/EBOOT.PBP
 
 # Remove symlink to game .pak when done
 rm -f /storage/openbor/Paks/Game.pak
+rm -f /storage/.config/retroarch/config/PPSSPP/EBOOT.cfg
