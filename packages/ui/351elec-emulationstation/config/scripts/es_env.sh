b4690828ed3a3bae557dd3bbb503df579e3a5ddb
Fewtarius <fewtarius@gmail.com>
2021-01-30 11:54:14 -0500

    * Update the environment path, and add /storage/bin for local binary or script adds.

--- packages/ui/351elec-emulationstation/config/scripts/es_env.sh
+++ packages/ui/351elec-emulationstation/config/scripts/es_env.sh
@@ -1,4 +1,4 @@
 HOME=/storage
 LD_LIBRARY_PATH=/usr/lib:/usr/lib/pulseaudio
-PATH=/usr/bin:/usr/bin:/usr/sbin:/usr/local/bin:/storage/.config/emulationstation/scripts:/storage/.config/usr/bin/batocera
+PATH=/usr/bin:/usr/sbin:/usr/local/bin:/storage/.config/emulationstation/scripts:/storage/bin
 #SDL_AUDIODRIVER=alsa
