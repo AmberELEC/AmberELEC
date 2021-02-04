0ee5744ba4a6b7027ac660ada25e8dcf82d5e8b7
Fewtarius <fewtarius@gmail.com>
2021-02-03 19:38:01 -0500

    Enable GLES for mpv

--- packages/multimedia/mpv/package.mk
+++ packages/multimedia/mpv/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="417b45c2ac88b9e020b55adf9238a4b46dc8c80d4c1a3efe301d55aaae4b0b99"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/mpv-player/mpv"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ffmpeg SDL2-git"
+PKG_DEPENDS_TARGET="toolchain ffmpeg SDL2-git ${OPENGLES}"
 PKG_LONGDESC="Video player based on MPlayer/mplayer2 https://mpv.io"
 PKG_TOOLCHAIN="manual"
 
@@ -16,7 +16,7 @@ configure_target() {
   # the bootstrap was failing for some reason. 
   cp $PKG_DIR/waf/* $PKG_BUILD  
   
-  ./waf configure --enable-sdl2 --enable-sdl2-gamepad --disable-pulse --enable-egl --disable-libbluray --disable-drm --disable-gl
+  ./waf configure --enable-sdl2 --enable-sdl2-gamepad --disable-pulse --enable-egl --disable-libbluray --disable-drm --disable-gl --opengl-es=yes
 }
 
 make_target() {
