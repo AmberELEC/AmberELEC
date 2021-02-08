68e0bb3e6013d5c4f210107f496650da140f244a
Fewtarius <fewtarius@gmail.com>
2021-02-08 07:33:14 -0500

    Fix port audio - missing SDL link broke it.

--- packages/tools/sound/mpg123/package.mk
+++ packages/tools/sound/mpg123/package.mk
@@ -1,5 +1,5 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="mpg123"
 PKG_VERSION="1.26.4"
@@ -7,13 +7,11 @@ PKG_VERSION="1.26.4"
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
 PKG_URL="http://www.mpg123.org/download/mpg123-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain alsa-lib"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git"
 PKG_LONGDESC="A console based real time MPEG Audio Player for Layer 1, 2 and 3."
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-                           --enable-static"
+PKG_BUILD_FLAGS="-fpic"
 
 if [ "$PULSEAUDIO_SUPPORT" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pulseaudio"
-  PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET} --with-default-audio=pulse --with-audio=alsa,pulse"
+  PKG_CONFIGURE_OPTS_TARGET="--with-default-audio=pulse --with-audio=alsa,pulse"
 fi
