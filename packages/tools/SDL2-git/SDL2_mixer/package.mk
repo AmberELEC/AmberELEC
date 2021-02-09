f386227ff27eb56bb96bc9afc2c1dbf9daf52e65
Fewtarius <fewtarius@gmail.com>
2021-02-08 19:37:20 -0500

    Fix broken dependency on libogg, libvorbis, flac

--- packages/tools/SDL2-git/SDL2_mixer/package.mk
+++ packages/tools/SDL2-git/SDL2_mixer/package.mk
@@ -7,16 +7,9 @@ PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git mpg123"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git libogg libvorbis flac mpg123"
 PKG_LONGDESC="SDL_mixer 2.0.1"
 
 pre_configure_target() {
-
-  PKG_CONFIGURE_OPTS_TARGET="--disable-fluidsynth \
-			 --disable-music-flac \
-			 --disable-music-mod-modplug \
-			 --disable-music-ogg-tremor \
-			 --enable-music-mp3"
-							 
-
+  PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET} --enable-music-mp3"
 }
