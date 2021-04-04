c53bf19fb997a7a5a6ad49510b32dd29f438cf65
Fewtarius <fewtarius@gmail.com>
2021-04-03 22:08:55 -0400

    Clean up device tools

--- projects/Rockchip/devices/RG351P/packages/odroidgoa-utils/package.mk
+++ projects/Rockchip/devices/RG351P/packages/odroidgoa-utils/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+# Copyright (C) 2021-present Fewtarius
 
 PKG_NAME="odroidgoa-utils"
 PKG_VERSION=""
@@ -9,7 +10,7 @@ PKG_LICENSE="OSS"
 PKG_DEPENDS_TARGET="toolchain rs97-commander-sdl2 python-evdev enable-oga-sleep"
 PKG_SITE=""
 PKG_URL=""
-PKG_LONGDESC="Support scripts for the ODROID-GO Advance"
+PKG_LONGDESC="Support scripts for the RG351P/M/V"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
@@ -18,12 +19,10 @@ makeinstall_target() {
 	cp battery.sh ${INSTALL}/usr/bin
         cp odroidgoa_utils.sh ${INSTALL}/usr/bin
 	cp volume_sense.sh ${INSTALL}/usr/bin
-        #cp oga_events.py $INSTALL/usr/bin
 }
 
 post_install() {  
-	#enable_service odroidgoa-hotkeys.service
 	enable_service volume.service
-	enable_service odroidgoa-headphones.service
+	enable_service headphones.service
 	enable_service battery.service
 }
