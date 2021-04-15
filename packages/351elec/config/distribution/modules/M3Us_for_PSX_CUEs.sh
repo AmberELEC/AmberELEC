bd6765aad09c4bf472074a030f028eccb1bdbf1b
Chris Farrell <7016191+seajaysec@users.noreply.github.com>
2021-04-15 17:02:02 -0400

    adding CCD

--- packages/351elec/config/distribution/modules/M3Us_for_PSX_CUEs.sh
+++ packages/351elec/config/distribution/modules/M3Us_for_PSX_CUEs.sh
@@ -8,12 +8,17 @@
 source /usr/bin/env.sh
 clear >/dev/console
 systemctl stop emustation
-message_stream "Making M3U's each PSX CUE. You may wish to hide CUE after."
+message_stream "Making M3U's each PSX CUE and CCD file."
 cd /storage/roms/psx/
-# Making m3u files for cue's
 for i in *.cue
 do
         title=$(echo "$i" | sed s'/.cue//g;s/ (Disc..)//g;s/ Disc..*$//g;s/ (.*//g')
+
+        echo "$i" >> "$title".m3u
+done
+for i in *.ccd
+do
+        title=$(echo "$i" | sed s'/.ccd//g;s/ (Disc..)//g;s/ Disc..*$//g;s/ (.*//g')
 
         echo "$i" >> "$title".m3u
 done
