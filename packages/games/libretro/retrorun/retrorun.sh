54dee35b5f9cf52795b631819a231ba6f66cb0c2
dhwz <dhwz@gmx.net>
2021-05-14 23:37:39 +0200

    add retrorun

---
+++ packages/games/libretro/retrorun/retrorun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo starting retrorun emulator...
+rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+/usr/bin/rg351p-js2xbox --silent -t oga_joypad &
+sleep 1
+ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+sleep 1
+echo starting game... "$1"
+sleep 1
+/usr/bin/retrorun --triggers -n -s /tmp/logs -d /roms/bios /tmp/cores/flycast_libretro.so "$1"
+sleep 1
+kill $(pidof rg351p-js2xbox)
+echo end!
