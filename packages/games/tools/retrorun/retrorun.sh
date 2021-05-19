28ddded25b7636fb028cbdba8bd69f98d2ad9eec
dhwz <dhwz@gmx.net>
2021-05-19 18:40:12 +0200

    merge fix retrorun.sh

--- packages/games/tools/retrorun/retrorun.sh
+++ packages/games/tools/retrorun/retrorun.sh
@@ -6,9 +6,10 @@ sleep 1
 ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sleep 1
-echo starting game... "$1"
+echo using core:"$1" 
+echo starting game... "$2"
 sleep 1
-/usr/bin/retrorun --triggers -n -s /tmp/logs -d /roms/bios /tmp/cores/flycast_libretro.so "$1"
+/usr/bin/retrorun --triggers -n -s /tmp/logs -d /roms/bios "$1" "$2"
 sleep 1
 kill $(pidof rg351p-js2xbox)
 echo end!
