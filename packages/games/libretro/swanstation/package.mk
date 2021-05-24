0781bac4eb0b39827872a62f02a9f8ff874a635e
XargonWan <xargonwan@gmail.com>
2021-05-24 14:25:29 +0200

    Removed duckstation and added swanstation as they changed core name and repo

---
+++ packages/games/libretro/swanstation/package.mk
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+# Maintenance 2020 351ELEC team (https://github.com/fewtarius/351ELEC)
+
+PKG_NAME="swanstation"
+PKG_VERSION="a39c7512c47c125f554c4e4725217c16628683e4"
+PKG_ARCH="aarch64"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/swanstation"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="SwanStation - PlayStation 1, aka. PSX Emulator"
+PKG_TOOLCHAIN="cmake"
+PKG_BUILD_FLAGS="-lto"
+
+pre_configure_target() {
+ PKG_CMAKE_OPTS_TARGET+=" -DCMAKE_BUILD_TYPE=Release -DBUILD_LIBRETRO_CORE=ON "
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/.$TARGET_NAME/swanstation_libretro.so $INSTALL/usr/lib/libretro/
+}
