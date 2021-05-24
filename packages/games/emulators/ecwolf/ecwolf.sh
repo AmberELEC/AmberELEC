2cc5289d613ca005539fca1a725816adf0b612cb
dhwz <dhwz@gmx.net>
2021-05-24 12:56:14 +0200

    ecwolf: improved mod file support

--- packages/games/emulators/ecwolf/ecwolf.sh
+++ packages/games/emulators/ecwolf/ecwolf.sh
@@ -2,6 +2,7 @@
 
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
+# Copyright (C) 2021-present 351ELEC (https://github.com/351ELEC/351ELEC)
 
 . /etc/profile
 
@@ -13,10 +14,10 @@ SAVE_DIR="/storage/roms/gamedata/ecwolf"
 params=" --config ${CONFIG_FILE} --savedir ${SAVE_DIR}"
 
 if [[ "$EE_DEVICE" == RG351P ]]; then
-        params+=" --res 480 320"
+  params+=" --res 480 320"
 fi
 if [[ "$EE_DEVICE" == RG351V ]]; then
-	params+=" --res 640 480"
+  params+=" --res 640 480"
 fi
 
 # data can be SD2 SD3 SOD WL6 or N3D and it's passed as the ROM
@@ -24,18 +25,30 @@ DATA=${1#*.}
 
 # If its a mod (extension .ecwolf) read the file and parse the data
 if [ ${DATA} == "ecwolf" ]; then
-    while IFS== read -r key value; do
-        if [ "$key" == "DATA" ]; then
-            params+=" --data $value"
-        fi
-
-        if [ "$key" == "PK3" ]; then
-            params+=" --file $value"
-        fi
-    done < "${1}"
+  dos2unix ${1}
+  while IFS== read -r key value; do
+  if [ "$key" == "DATA" ]; then
+    params+=" --data $value"
+  fi
+  if [ "$key" == "PK3" ]; then
+    params+=" --file $value"
+  fi
+  if [ "$key" == "PK3_1" ]; then
+    params+=" --file $value"
+  fi
+  if [ "$key" == "PK3_2" ]; then
+    params+=" --file $value"
+  fi
+  if [ "$key" == "PK3_3" ]; then
+    params+=" --file $value"
+  fi
+  if [ "$key" == "PK3_4" ]; then
+    params+=" --file $value"
+  fi
+  done < "${1}"
 else
-    params+=" --data ${DATA}"
+  params+=" --data ${DATA}"
 fi
 
 cd "${CONFIG_DIR}"
-ecwolf ${params} > /tmp/logs/ecwolf.log 2>&1 
+ecwolf ${params} > /tmp/logs/ecwolf.log 2>&1
