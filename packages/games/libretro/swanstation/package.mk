1e8dcacf78615ab282c32e626b96c794e518b834
dhwz <dhwz@gmx.net>
2021-05-24 14:19:57 +0200

    update swanstation

--- packages/games/libretro/swanstation/package.mk
+++ packages/games/libretro/swanstation/package.mk
@@ -3,15 +3,14 @@
 # Maintenance 2020 351ELEC team (https://github.com/fewtarius/351ELEC)
 
 PKG_NAME="swanstation"
-PKG_VERSION="5b6a1706e4f381bd0b85185a11fd487ecc6b4757"
-PKG_SHA256="b95fdedd24a9ff473606830e56a7245dde779882f14ff9e1ff153b077c56042b"
+PKG_VERSION="a39c7512c47c125f554c4e4725217c16628683e4"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/duckstation"
+PKG_SITE="https://github.com/libretro/swanstation"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
 PKG_SECTION="libretro"
-PKG_SHORTDESC="DuckStation - PlayStation 1, aka. PSX Emulator"
+PKG_SHORTDESC="SwanStation - PlayStation 1, aka. PSX Emulator"
 PKG_TOOLCHAIN="cmake"
 PKG_BUILD_FLAGS="-lto"
 
@@ -21,5 +20,5 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/.$TARGET_NAME/duckstation_libretro.so $INSTALL/usr/lib/libretro/swanstation_libretro.so
+  cp $PKG_BUILD/.$TARGET_NAME/swanstation_libretro.so $INSTALL/usr/lib/libretro/
 }
