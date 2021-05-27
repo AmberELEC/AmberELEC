6c2e29ce9e2feeaee7414361dd25fbe9ad0905fd
dhwz <dhwz@gmx.net>
2021-05-27 21:59:35 +0200

    add easyrpg distribution data

--- packages/games/libretro/easyrpg/package.mk
+++ packages/games/libretro/easyrpg/package.mk
@@ -49,4 +49,39 @@ pre_make_target() {
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
   cp $PKG_BUILD/.$TARGET_NAME/easyrpg_libretro.so $INSTALL/usr/lib/libretro/
+
+  mkdir -p $INSTALL/usr/config/distribution/easyrpg
+  INSTALLPATH=$INSTALL/usr/config/distribution/easyrpg
+  SOURCEPATH="$(pwd)"
+  BIOS_URL="https://dl.degica.com/rpgmakerweb/run-time-packages"
+  install_bios rpg2000_rtp_installer.exe rtp/2000
+  install_bios rpg2003_rtp_installer.zip rtp/2003
+}
+
+install_bios() {
+  BIOS="$1"
+  BIOSPATH="$2"
+  if [ -d "${INSTALLPATH}/${BIOSPATH}" ]
+  then
+    rm -rf "${INSTALLPATH}/${BIOSPATH}"
+  fi
+  mkdir -p "${INSTALLPATH}/${BIOSPATH}"
+  cd "${INSTALLPATH}/${BIOSPATH}"
+  curl -Lo "${BIOS}" "${BIOS_URL}/${BIOS}"
+  BINSUM=$(sha256sum "${BIOS}" | awk '{print $1}')
+  SHASUM=$(cat $PKG_DIR/SHA256SUMS | awk '/'${BIOS}'/ {print $1}')
+  if [ ! "${SHASUM}" == "${BINSUM}" ]
+  then
+    echo "Checksum mismatch, please update the package."
+    exit 1
+  fi
+  if [[ "${BIOS}" =~ .zip$ ]]
+  then
+    7z x "${BIOS}"
+    rm "${BIOS}"
+    BIOS="$(echo ${BIOS} | sed "s#.zip#.exe#")"
+  fi
+  7z x "${BIOS}"
+  rm "${BIOS}"
+  cd ${SOURCEPATH}
 }
