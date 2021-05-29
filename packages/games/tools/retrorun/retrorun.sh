a513624fd9e63bb536b743da204921424ec23fbb
navy1978 <ivan.morelli@gmail.com>
2021-05-29 17:40:34 +0200

    it adds show_fps optional parameter to retrorun.sh

--- packages/games/tools/retrorun/retrorun.sh
+++ packages/games/tools/retrorun/retrorun.sh
@@ -9,9 +9,11 @@ ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joyst
 chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sleep 1
 echo 'using core:' "$1"
+echo 'platform:' "$3"
 echo 'starting game:' "$2"
+
 FPS=''
-if [[ "$3" == "show_fps" ]] || [[ "$3" == "SHOW_FPS" ]]
+if [[ "$4" == "show_fps" ]] || [[ "$4" == "SHOW_FPS" ]]
 then
     echo 'enabling FPS in the logs'
     FPS="-f"
@@ -21,10 +23,10 @@ if [[ "$1" =~ "pcsx_rearmed" ]] || [[ "$1" =~ "parallel_n64" ]] || [[ "$1" =~ "u
 then
     echo 'using 32bit'
   	export LD_LIBRARY_PATH="/usr/lib32"
-	/usr/bin/retrorun32 --triggers $FPS -n -s /tmp/logs -d /roms/bios -f "$1" "$2"
+	/usr/bin/retrorun32 --triggers $FPS -n -s /storage/roms/"$3"  -d /roms/bios -f "$1" "$2"
 else
 	echo 'using 64bit'
-	/usr/bin/retrorun --triggers $FPS -n -s /roms/n64 -d /roms/bios "$1" "$2"
+	/usr/bin/retrorun --triggers $FPS -n -s /storage/roms/"$3" -d /roms/bios "$1" "$2"
 fi
 sleep 1
 rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
