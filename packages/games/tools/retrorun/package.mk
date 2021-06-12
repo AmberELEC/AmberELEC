0b47e23b4654313e715eade3cbb120196d9a081d
navy1978 <ivan.morelli@gmail.com>
2021-06-12 03:59:57 -0400

    adds FPS on screen to Retrorun (SELECT + Y)

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retrorun"
-PKG_VERSION="a8fabc27780502a3ebc41e6b79f8e7ab74e3d1f7"
+PKG_VERSION="5ec465911cd1082e380deb29cec7d145ea05c214"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun-go2"
