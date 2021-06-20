af8b54895e2bf1f119fb4066a7b599c641e1b605
konsumschaf <github@konsumschaf.de>
2021-06-20 13:59:42 +0200

    No need to copy the bezels in the user space every time

--- packages/351elec/sources/autostart.sh
+++ packages/351elec/sources/autostart.sh
@@ -99,11 +99,13 @@ rsync --ignore-existing -raz /usr/config/remappings/* /storage/remappings/ &
 # Copy OpenBOR
 rsync --ignore-existing -raz /usr/config/openbor /storage &
 
-# copy bezel if it doesn't exists
-if [ ! -f "/storage/roms/bezels/default.cfg" ]; then
-  mkbezels/
-  rsync --ignore-existing -raz /usr/share/retroarch-overlays/bezels/* /storage/roms/bezels/ &
-fi
+## Not needed any more
+## copy bezel if it doesn't exists
+#if [ ! -f "/storage/roms/bezels/default.cfg" ]; then
+#  mkbezels/
+#  rsync --ignore-existing -raz /usr/share/retroarch-overlays/bezels/* /storage/roms/bezels/ &
+#fi
+##
 
 # Copy pico-8
 cp -f  "/usr/bin/pico-8.sh" "/storage/roms/pico-8/Start Pico-8.sh" &
