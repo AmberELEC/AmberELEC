88ca7a8e797649fb837f9d1350748c212a63f0d7
navy1978 <ivan.morelli@gmail.com>
2021-06-27 20:13:12 +0000

    it fixes retrorun problem on V (black screen)

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retrorun"
-PKG_VERSION="a8fabc27780502a3ebc41e6b79f8e7ab74e3d1f7"
+PKG_VERSION="0ec798f493bfb6cb6b11f4e3ac71cee88bec27e1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun-go2"
