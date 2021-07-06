12b62547f44c63a788dec9e5a1a01a72b0d5d7b5
pkegg <30480682+pkegg@users.noreply.github.com>
2021-07-06 09:18:36 -0600

    Reenable updates (#510)
    * Make the script be able to run in a test mode so less painful to test
    
    fixes from running on real device
    
    Refactor to use deduplication update check and update
    
    Switch to pkegg emustation to test
    
    Update package.mk
    
    Update packages
    
    Some fixes - switch to pkegg repo for now to test updates
    
    Update logging
    
    Add python script - not hooked up yet
    
    Update emulation station
    
    Make beta channel work
    
    Update to revert the update notifications
    
    better get release implementation - reimplement everything (almost in python)
    
    better error handling
    
    better timeout
    
    updates from running on real device
    
    Refactor bash in progress
    
    finish python port
    
    Also add init toolchain
    
    formatting
    
    Formatting
    
    refactor
    
    refactor
    
    More refactoring
    
    refactor
    
    docs
    
    update to new version of pkegg es
    
    remove toolchain
    
    cleanup
    
    * Update from real device
    
    * cleanup
    
    * cleanup
    
    * Fix reboot not working properly
    
    * Remove hard coding to pkegg updates and add a developer option to set the github org/repo.  Also add a force-update developer option which will always detect and download the latest release (for testing)
    
    * Fix logging output to file for debugging purposes
    
    * undo inadvertant makefile changes
    
    * Switch to used 351elec ES with re-enable updates changes in it
    
    Co-authored-by: pkegg <>
    Co-authored-by: pkegg <pkegg>

--- packages/addons/addon-depends/bash/package.mk
+++ packages/addons/addon-depends/bash/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="604d9eec5e4ed5fd2180ee44dd756ddca92e0b6aa4217bbab2b6227380317f23"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/bash/bash.html"
 PKG_URL="ftp://ftp.cwru.edu/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_DEPENDS_TARGET="toolchain ncurses readline"
 PKG_LONGDESC="The GNU Bourne Again shell."
 
 PKG_CONFIGURE_OPTS_TARGET="--with-curses \
