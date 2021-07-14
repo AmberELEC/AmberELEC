32df0c90f36bc6225c6c87a39e8f9d464230d43d
YonKuma <kumarei@gmail.com>
2021-07-14 07:10:50 -0400

    Environment variables to build

--- packages/games/emulators/mupen64plus-core/package.mk
+++ packages/games/emulators/mupen64plus-core/package.mk
@@ -7,27 +7,22 @@ PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-core/releases/download/2.5.9/mupen64plus-core-src-2.5.9.tar.gz"
 PKG_SHA256="84244c77a93c0c198729da06d75628c5c5513ee56a5fc0a5785a92461071f34c"
-PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng SDL2 zlib freetype"
+PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host"
 PKG_SHORTDESC="mupen64plus"
 PKG_LONGDESC="Mupen64Plus Standalone"
 PKG_TOOLCHAIN="manual"
 
 PKG_MAKE_OPTS_TARGET+="USE_GLES=1"
 
-post_unpack() {
-  mkdir -p ${PKG_UNPACK_DIR}
-  mv ${PKG_BUILD} "${PKG_UNPACK_DIR}/.intermediate"
-  mkdir -p "${PKG_BUILD}/source"
-  mv ${PKG_UNPACK_DIR}/.intermediate ${PKG_BUILD}/source/mupen64plus-core
-  rm -rf "${PKG_UNPACK_DIR}"
-}
-
-pre_make_target() {
-  export CFLAGS=${CFLAGS}
-  export CPPFLAGS=${CPPFLAGS}
-}
-
 make_target() {
-  make -C source/mupen64plus-core/projects/unix all ${PKG_MAKE_OPTS_TARGET}
+  export HOST_CPU=aarch64
+  export USE_GLES=1
+  export SDL_CFLAGS="-I$SYSROOT_PREFIX/usr/include/SDL2 -D_REENTRANT"
+  export SDL_LDLIBS="-lSDL_net -lSDL2"
+  export CROSS_COMPILE="$TARGET_PREFIX"
+  export V=1
+  export OPTFLAGS="--allow-multiple-definition"
+  make -C projects/unix clean
+  make -C projects/unix all ${PKG_MAKE_OPTS_TARGET}
 }
 
