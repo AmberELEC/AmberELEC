e69d176d00af5e927c20454a2f77566c74d06660
YonKuma <kumarei@gmail.com>
2021-07-14 07:10:50 -0400

    Environment variables to build

--- packages/virtual/image/package.mk
+++ packages/virtual/image/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="image"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
-PKG_DEPENDS_TARGET="toolchain squashfs:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts network misc-packages debug usb-modeswitch unzip poppler socat p7zip file mupen64plus-core"
+PKG_DEPENDS_TARGET="toolchain squashfs:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts network misc-packages debug usb-modeswitch unzip poppler socat p7zip file"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Root package used to build and create complete image"
 
