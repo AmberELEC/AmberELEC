d934325f74b608e13dc22e7b74093950bd8699cc
YonKuma <kumarei@gmail.com>
2021-07-15 17:29:44 -0400

    mupen64plus-core building

--- packages/games/emulators/mupen64plus-core/package.mk
+++ packages/games/emulators/mupen64plus-core/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mupen64plus-core"
-PKG_VERSION="1.0"
+PKG_VERSION="c44352d72dc05005bd23e826e0acb4267aa00719"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_URL="https://github.com/mupen64plus/mupen64plus-core/releases/download/2.5.9/mupen64plus-core-src-2.5.9.tar.gz"
-PKG_SHA256="84244c77a93c0c198729da06d75628c5c5513ee56a5fc0a5785a92461071f34c"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-core/archive/${PKG_VERSION}.tar.gz"
+PKG_SHA256="9bb29bd87a421f549a838bc181fd8adab1a434e8877d3064fa7d057ab5bcab7f"
 PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host"
 PKG_SHORTDESC="mupen64plus"
 PKG_LONGDESC="Mupen64Plus Standalone"
@@ -21,8 +21,22 @@ make_target() {
   export SDL_LDLIBS="-lSDL_net -lSDL2"
   export CROSS_COMPILE="$TARGET_PREFIX"
   export V=1
-  export OPTFLAGS="--allow-multiple-definition"
+  export NEW_DYNAREC=1
   make -C projects/unix clean
-  make -C projects/unix all ${PKG_MAKE_OPTS_TARGET}
+  make -C projects/unix PREFIX=${INSTALL}/usr/local all ${PKG_MAKE_OPTS_TARGET}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/local/lib
+  cp ${PKG_BUILD}/projects/unix/libmupen64plus.so.2.0.0 ${INSTALL}/usr/local/lib
+  $STRIP ${INSTALL}/usr/local/lib/libmupen64plus.so.2.0.0
+  chmod 644 ${INSTALL}/usr/local/lib/libmupen64plus.so.2.0.0
+  ln -sf ${INSTALL}/usr/local/lib/libmupen64plus.so.2.0.0 ${INSTALL}/usr/local/lib/libmupen64plus.so.2
+  mkdir -p ${INSTALL}/usr/local/share/mupen64plus
+  cp ${PKG_BUILD}/data/* ${INSTALL}/usr/local/share/mupen64plus
+  chmod 0644 ${INSTALL}/usr/local/share/mupen64plus/*
+  mkdir -p ${INSTALL}/usr/include/mupen64plus
+  cp ${PKG_BUILD}/src/api/m64p_*.h ${INSTALL}/usr/include/mupen64plus
+  chmod 0644 ${INSTALL}/usr/include/mupen64plus/*
 }
 
