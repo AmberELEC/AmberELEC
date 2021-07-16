91a3340120dd34a74a037fd6c3a64dba9c5789b5
YonKuma <kumarei@gmail.com>
2021-07-16 18:12:51 -0400

    Added mupen to elec build

---
+++ packages/virtual/mupen64plus-standalone/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="mupen64plus-standalone"
+PKG_VERSION=""
+PKG_LICENSE="GPL"
+PKG_SITE="https://mupen64plus.org/"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain mupen64plus-core mupen64plus-ui-console mupen64plus-audio-sdl mupen64plus-rsp-hle mupen64plus-video-rice mupen64plus-video-glide64mk2"
+PKG_SECTION="virtual"
+PKG_LONGDESC="Meta package for installing the different Mupen64Plus standalone modules"
+
