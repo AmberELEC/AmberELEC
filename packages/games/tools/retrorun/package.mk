feabd6937b33a664cec37c77fa25da9549fc9986
navy1978 <ivan.morelli@gmail.com>
2021-07-18 16:48:14 +0000

    fixes issue in retrorun screenshots (wrong resolution on P/M)

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retrorun"
-PKG_VERSION="d4f280ed69f7295cde117ea6642a11a788b35a47"
+PKG_VERSION="79bc02eefbb38ca9ac2afdb8846a7769ba6f591b"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun-go2"
