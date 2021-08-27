415e701d6ae726b3207ac1a36d1fdc7705d4c483
YonKuma <kumarei@gmail.com>
2021-08-27 16:35:58 -0400

    Config and working emulator

--- packages/games/emulators/mupen64plus-standalone/mupen64plus-core/m64p.sh
+++ packages/games/emulators/mupen64plus-standalone/mupen64plus-core/m64p.sh
@@ -2,7 +2,12 @@
 
 if [[ ! -f "/storage/roms/gamedata/mupen64plus/InitAutoCfg.ini" ]]; then
 	mkdir -p /storage/roms/gamedata/mupen64plus
-	cp /storage/.config/mupen64plus/InitAutoCfg.ini /storage/roms/gamedata/mupen64plus/
+	cp /usr/local/share/mupen64plus/InputAutoCfg.ini /storage/roms/gamedata/mupen64plus/
+fi
+
+if [[ ! -f "/storage/.corfig/mupen64plus/mupen64plus.cfg" ]]; then
+	mkdir -p /storage/.config/mupen64plus
+	cp /usr/local/share/mupen64plus/mupen64plus.cfg /storage/.config/mupen64plus/
 fi
 
 /usr/local/bin/mupen64plus --gfx mupen64plus-video-$1 "$2"
