4cfc6d16c33d4cd20f8472b74271db4945d35c74
YonKuma <kumarei@gmail.com>
2021-08-28 10:11:03 -0400

    Updated name to mupen64plusa

--- packages/games/emulators/mupen64plusa/mupen64plusa-rsp-hle/package.mk
+++ packages/games/emulators/mupen64plusa/mupen64plusa-rsp-hle/package.mk
@@ -1,22 +1,22 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
-PKG_NAME="mupen64plus-audio-sdl"
-PKG_VERSION="af6af5b1fd4fdb435c836be15371dd047f395c4d"
+PKG_NAME="mupen64plusa-rsp-hle"
+PKG_VERSION="88766c6e2d30492e64d78999825068aa598c1080"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_URL="https://github.com/mupen64plus/mupen64plus-audio-sdl/archive/${PKG_VERSION}.tar.gz"
-PKG_SHA256="265ccdd56736f9cb4144196ad6673d2728a8ebd1e7b9c4280a632c3a343be261"
-PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host mupen64plus-core"
-PKG_SHORTDESC="mupen64plus-audio-sdl"
-PKG_LONGDESC="Mupen64Plus Standalone Audio SDL"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-rsp-hle/archive/${PKG_VERSION}.tar.gz"
+PKG_SHA256="cfd80dacc3f94a00f01e476717df7491beeff934007dbcdb84a1a5c6d97a22c0"
+PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host mupen64plusa-core"
+PKG_SHORTDESC="mupen64plus-rsp-hle"
+PKG_LONGDESC="Mupen64Plus Standalone RSP HLE"
 PKG_TOOLCHAIN="manual"
 
 PKG_MAKE_OPTS_TARGET+="USE_GLES=1"
 
 make_target() {
   export HOST_CPU=aarch64
-  export APIDIR=$(get_build_dir mupen64plus-core)/.install_pkg/usr/local/include/mupen64plus
+  export APIDIR=$(get_build_dir mupen64plusa-core)/.install_pkg/usr/local/include/mupen64plus
   export USE_GLES=1
   export SDL_CFLAGS="-I$SYSROOT_PREFIX/usr/include/SDL2 -D_REENTRANT"
   export SDL_LDLIBS="-lSDL2_net -lSDL2"
@@ -33,8 +33,8 @@ makeinstall_target() {
   ULIBDIR=${UPREFIX}/lib
   UPLUGINDIR=${ULIBDIR}/mupen64plus
   mkdir -p ${UPLUGINDIR}
-  cp ${PKG_BUILD}/projects/unix/mupen64plus-audio-sdl.so ${UPLUGINDIR}
-  #$STRIP ${UPLUGINDIR}/mupen64plus-audio-sdl.so
-  chmod 0644 ${UPLUGINDIR}/mupen64plus-audio-sdl.so
+  cp ${PKG_BUILD}/projects/unix/mupen64plus-rsp-hle.so ${UPLUGINDIR}
+  #$STRIP ${UPLUGINDIR}/mupen64plus-rsp-hle.so
+  chmod 0644 ${UPLUGINDIR}/mupen64plus-rsp-hle.so
 }
 
--- packages/games/emulators/mupen64plusa/mupen64plusa-video-rice/package.mk
+++ packages/games/emulators/mupen64plusa/mupen64plusa-video-rice/package.mk
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
-PKG_NAME="mupen64plus-video-rice"
+PKG_NAME="mupen64plusa-video-rice"
 PKG_VERSION="8cd53f0272cd3cd44510a01fd0aedcb7e5df0790"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-video-rice/archive/${PKG_VERSION}.tar.gz"
 PKG_SHA256="3a7fa1da2a7b58862057d7e18910be611a11474ea82dfcc9a3e4b52c03b56007"
-PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host mupen64plus-core"
+PKG_DEPENDS_TARGET="toolchain ${OPENGLES} libpng17 SDL2 SDL2_net zlib freetype nasm:host mupen64plusa-core"
 PKG_SHORTDESC="mupen64plus-video-rice"
 PKG_LONGDESC="Mupen64Plus Standalone Rice Video Driver"
 PKG_TOOLCHAIN="manual"
@@ -16,7 +16,7 @@ PKG_MAKE_OPTS_TARGET+="USE_GLES=1"
 
 make_target() {
   export HOST_CPU=aarch64
-  export APIDIR=$(get_build_dir mupen64plus-core)/.install_pkg/usr/local/include/mupen64plus
+  export APIDIR=$(get_build_dir mupen64plusa-core)/.install_pkg/usr/local/include/mupen64plus
   export USE_GLES=1
   export SDL_CFLAGS="-I$SYSROOT_PREFIX/usr/include/SDL2 -D_REENTRANT"
   export SDL_LDLIBS="-lSDL2_net -lSDL2"
