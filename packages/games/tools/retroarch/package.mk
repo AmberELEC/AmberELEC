10d648ece2850ef2c7bf55c89543efa75040270c
dhwz <dhwz@gmx.net>
2021-09-11 20:47:10 +0200

    bump retroarch to 1.9.9

--- packages/games/tools/retroarch/package.mk
+++ packages/games/tools/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="4a6dc36b1cc026bf337537e2d7527a117727bb91"
+PKG_VERSION="36888612eb07b6d1ed68cc1079ef3af2c730a112"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="$PKG_SITE.git"
 PKG_LICENSE="GPLv3"
