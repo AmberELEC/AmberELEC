7ae5d79ce327af97d6a76984da0f25e962150025
Megan Leet <megan.leet@iinet.net.au>
2021-09-17 23:58:55 +1000

    Solarus: Use double quotes in launch script
    The most trivial pull request ever! :D
    
    This is needed for being able to launch Solarus games from a path that contains spaces.

--- packages/games/emulators/solarus/scripts/solarus.sh
+++ packages/games/emulators/solarus/scripts/solarus.sh
@@ -4,6 +4,6 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 [[ ! -d "/storage/roms/gamedata/solarus/" ]] && mkdir -p "/storage/roms/gamedata/solarus/"
-solarus-run -fullscreen=yes -joypad-deadzone=32767 -quit-combo=6+7 ${1}
+solarus-run -fullscreen=yes -joypad-deadzone=32767 -quit-combo=6+7 "${1}"
 
 exit 0
