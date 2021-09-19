d3efc0326cfbff026f253a5629f978c356e2f9c3
dhwz <dhwz@gmx.net>
2021-09-19 17:56:45 +0200

    bump mpg123

--- packages/tools/sound/mpg123/package.mk
+++ packages/tools/sound/mpg123/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="mpg123"
-PKG_VERSION="1.28.0"
+PKG_VERSION="1.29.0"
 #PKG_SHA256=""
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
