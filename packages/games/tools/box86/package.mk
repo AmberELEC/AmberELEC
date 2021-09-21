383d11e73ad8733a38bf2429e4e3c31ca613534d
dhwz <dhwz@gmx.net>
2021-09-21 17:16:52 +0200

    bump box86/box64

--- packages/games/tools/box86/package.mk
+++ packages/games/tools/box86/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2020-present 351ELEC (https://github.com/351ELEC)
 
 PKG_NAME="box86"
-PKG_VERSION="e9cc8d43a9ac922795ad02fd06b3d492e700c3ce"
+PKG_VERSION="844566a78ae3c995c3c575d5822616e5e783fe39"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="MIT"
@@ -13,7 +13,7 @@ PKG_DEPENDS_TARGET="toolchain gl4es"
 PKG_LONGDESC="Box86 - Linux Userspace x86 Emulator with a twist, targeted at ARM Linux devices"
 PKG_TOOLCHAIN="cmake"
 
-PKG_CMAKE_OPTS_TARGET=" -DRK3326=1 -DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_TARGET=" -DGOA_CLONE=1 -DCMAKE_BUILD_TYPE=Release"
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/share/box86/lib
