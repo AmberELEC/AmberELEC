86e87a6342fa9185882f75f812f8d990884c1cd9
dhwz <dhwz@gmx.net>
2021-09-26 22:40:42 +0200

    fix mupen64plus-core package

--- packages/games/emulators/mupen64plussa/mupen64plussa-core/package.mk
+++ packages/games/emulators/mupen64plussa/mupen64plussa-core/package.mk
@@ -40,7 +40,11 @@ makeinstall_target() {
   cp ${PKG_BUILD}/src/api/m64p_*.h ${INSTALL}/usr/local/include/mupen64plus
   chmod 0644 ${INSTALL}/usr/local/include/mupen64plus/*
 
-  cp ${PKG_DIR}/config/mupen64plus-${DEVICE}.cfg ${INSTALL}/usr/local/share/mupen64plus/mupen64plus.cfg
+  if [ "${DEVICE}" = "RG351P" ]; then
+    cp ${PKG_DIR}/config/mupen64plus-RG351P.cfg ${INSTALL}/usr/local/share/mupen64plus/mupen64plus.cfg
+  elif [ "${DEVICE}" = "RG351V" ]; then
+    cp ${PKG_DIR}/config/mupen64plus-RG351V.cfg ${INSTALL}/usr/local/share/mupen64plus/mupen64plus.cfg
+  fi
   chmod 644 ${INSTALL}/usr/local/share/mupen64plus/mupen64plus.cfg
   mkdir -p ${INSTALL}/usr/bin
   cp ${PKG_DIR}/m64p.sh ${INSTALL}/usr/bin
