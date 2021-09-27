75740d6f06206f50c184d0ad4b9faf6e4e9a1ba3
dhwz <dhwz@gmx.net>
2021-09-27 17:15:09 +0200

    351Files add RG351MP support

--- packages/tools/sysutils/351files/package.mk
+++ packages/tools/sysutils/351files/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present 351ELEC (https://github.com/351elec)
 
 PKG_NAME="351files"
-PKG_VERSION="492961726abb04ebefa58f7dda47b7040f3bd088"
+PKG_VERSION="bab7144d2b3dccdc090445608d0a21210e3d438b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -12,6 +12,11 @@ PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_image SDL2_gfx SDL2_ttf"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="A Single panel file Manager tailored for Anbernic 351 devices: RG351V/MP and RG351P/M. Can be easily adapted to any Linux-based device."
+
+if [ "${DEVICE}" = "RG351MP" ]
+then
+  PKG_PATCH_DIRS="RG351MP"
+fi
 
 make_target() {
   make DEVICE=${DEVICE} RES_PATH=/usr/share/351files/res START_PATH=/storage/roms SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config CC=$CXX
