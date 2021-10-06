71daae18e5b0dcb84e418b188496cbc0c68e8784
Szalik <77732736+szalik-rg351@users.noreply.github.com>
2021-10-06 20:38:13 +0200

    Bump Art-Book

--- packages/ui/351elec-emulationstation/themes/es-theme-art-book-4-3/package.mk
+++ packages/ui/351elec-emulationstation/themes/es-theme-art-book-4-3/package.mk
@@ -4,7 +4,7 @@
 
 PKG_NAME="es-theme-art-book-4-3"
 PKG_REV="1"
-PKG_VERSION="3c26cdaec68145225d112c59ecfec7e944af1ee1"
+PKG_VERSION="e8f6f428f696d09616e282bcc6d818cfa982f525"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/szalik-rg351/es-theme-art-book-4-3"
