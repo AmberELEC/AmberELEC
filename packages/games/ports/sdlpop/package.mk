d8bbadaa78429435d16f8a7b9b199c92a4dec46a
dhwz <dhwz@gmx.net>
2021-10-13 22:57:41 +0200

    bump sdlpop

--- packages/games/ports/sdlpop/package.mk
+++ packages/games/ports/sdlpop/package.mk
@@ -2,8 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="sdlpop"
-PKG_VERSION="28741e15eab909c11c5a78c0a8a80c1004903dbb"
-PKG_SHA256="0a5b81729fd1b69e5029a62429a003e5ccc0d0a325881970aec9095036db6190"
+PKG_VERSION="817492fb28909423889f6013cdee0e26c26f325a"
 PKG_ARCH="any"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/NagyD/SDLPoP"
@@ -14,24 +13,28 @@ PKG_LONGDESC="An open-source port of Prince of Persia, based on the disassembly 
 PKG_TOOLCHAIN="make"
 
 pre_configure_target() {
-sed -i "s/start_fullscreen = false/start_fullscreen = true/" "$PKG_BUILD/SDLPoP.ini"
-sed -i "s/enable_info_screen = true;/enable_info_screen = false;/" "$PKG_BUILD/SDLPoP.ini"
+  sed -i "s/start_fullscreen = false/start_fullscreen = true/" "$PKG_BUILD/SDLPoP.ini"
+  sed -i "s/enable_info_screen = true;/enable_info_screen = false;/" "$PKG_BUILD/SDLPoP.ini"
+}
+
+pre_make_target() {
+  CFLAGS+=" -I$(get_build_dir SDL2)/include -I$(get_build_dir SDL2_image) -D_REENTRANT"
 }
 
 make_target() {
-	cd $PKG_BUILD/src/
-	mkdir -p build
-	cd build/
-	cmake ..
-	make CC=cc
+  cd $PKG_BUILD/src/
+  mkdir -p build
+  cd build/
+  cmake ..
+  make CC=cc
 }
 
 makeinstall_target() {
-	mkdir -p $INSTALL/usr/config/distribution/configs/SDLPoP
-	mkdir -p $INSTALL/usr/local/bin
-	cp -r $PKG_BUILD/* $INSTALL/usr/config/distribution/configs/SDLPoP/
-	mv "$INSTALL/usr/config/distribution/configs/SDLPoP/prince" "$INSTALL/usr/local/bin/"
-	rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/src
-	rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/.gitignore
-	rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/.editorconfig
+  mkdir -p $INSTALL/usr/config/distribution/configs/SDLPoP
+  mkdir -p $INSTALL/usr/local/bin
+  cp -r $PKG_BUILD/* $INSTALL/usr/config/distribution/configs/SDLPoP/
+  mv "$INSTALL/usr/config/distribution/configs/SDLPoP/prince" "$INSTALL/usr/local/bin/"
+  rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/src
+  rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/.gitignore
+  rm -rf $INSTALL/usr/config/distribution/configs/SDLPoP/.editorconfig
 }
