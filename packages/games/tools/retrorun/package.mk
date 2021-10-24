35080a9105c4ee2b0e80e3025b438a3cc63e480a
navy1978 <navy1978@gmail.com>
2021-10-24 09:37:16 +0000

    another test for retrorun

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retrorun"
-PKG_VERSION="d05440a1ef4cf3fe7cba1a0760400ed126fee523"
+PKG_VERSION="e3596aa3567471944ea7a4dd6b55d782afab6386"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/351ELEC/retrorun-go2"
+PKG_SITE="https://github.com/navy1978/retrorun-go2"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain libgo2 libdrm libpng linux"
 PKG_TOOLCHAIN="make"
