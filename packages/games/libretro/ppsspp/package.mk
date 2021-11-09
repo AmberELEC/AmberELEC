5636595a26699ca0556f592778bde0ca16aac0a4
dhwz <dhwz@gmx.net>
2021-11-09 21:20:58 +0100

    bump ppsspp

--- packages/games/libretro/ppsspp/package.mk
+++ packages/games/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="1f502b8f948c9607a3d5127531461878752b5d81"
+PKG_VERSION="4d3890547464bd184ed58b21e3d9c4b80dec743d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
