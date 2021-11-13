fc5044908b18451073284040b9a7c557d501698a
navy1978 <navy1978@gmail.com>
2021-11-13 14:51:41 +0000

    testign new input events in libgo2

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retrorun"
-PKG_VERSION="05d0cdb6f5f82ca000585031bfe1a7cf22c8afaa"
+PKG_VERSION="89f658f4bb2ccb9a000744674d2dc3ce21a1ebc1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/351ELEC/retrorun-go2"
+PKG_SITE="https://github.com/navy1978/retrorun-go2"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain libgo2 libdrm libpng linux"
 PKG_TOOLCHAIN="make"
