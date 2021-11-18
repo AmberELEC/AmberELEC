162157dc3b8c8d916ef9c48c6e720fdc2947473f
dhwz <dhwz@gmx.net>
2021-11-18 22:23:11 +0100

    rollback libfmt to 5.3.0 (fixes easyrpg 0.7.0)

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -2,12 +2,13 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfmt"
-PKG_VERSION="8.0.1"
-PKG_SHA256="b06ca3130158c625848f3fb7418f235155a4d389b2abc3a6245fb01cb0eb1e01"
+PKG_VERSION="5.3.0" # newer version break easyrpg!
+PKG_SHA256="defa24a9af4c622a7134076602070b45721a43c51598c8456ec6f2c4dbb51c89"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
-PKG_URL="https://github.com/fmtlib/fmt/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/fmtlib/fmt/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="fmt is an open-source formatting library for C++. It can be used as a safe alternative to printf or as a fast alternative to IOStreams."
+PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_EXTENSIONS:BOOL=OFF -DFMT_DOC=OFF -DFMT_INSTALL=ON -DFMT_TEST=OFF"
+PKG_CMAKE_OPTS_TARGET="-DFMT_DOC=OFF -DFMT_INSTALL=ON -DFMT_TEST=OFF -DFMT_USE_CPP11=ON"
