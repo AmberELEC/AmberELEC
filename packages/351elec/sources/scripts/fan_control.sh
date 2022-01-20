0b65c87dcedb465d9721e197cc5db7b8cf7ade27
pkegg <pkegg@users.noreply.github.com>
2022-01-20 15:32:18 -0700

    Implement fan control profiles
    Fan speed is evaluated every 15 seconds.  Fan profiles will change at the next 15 second interval after the value is changed (ex: when ES system settings screen is exited) and does not require a reboot.  The code is careful to only reload updated distribution.conf settings when distribution.conf changes to avoid any CPU/disk overhead.
    
    Fan speeds can be monitored on the device using `journalctl -f`.  In the future, fan speeds may not be logged as often (logged every 15 seconds right now).
    
    NOTE: Temperature is in degrees Celsius * 1000.  So 40C is 40000.  Fan speed will scale linearly between fan min temp to fan max temp.
    
    Valid profiles:
    - default - balance of fan noise and heat.  Fan should start during most gaming and scale up with CPU
      - For: most users who don't care about the fan or heat and just want it to work.
      - fan max speed: 255 (100%)
      - fan min temp: 40000
      - fan max temp: 70000
      - NOTE: This profile will be used if there is no value in distribution.conf or any other value than 'performance' or 'quiet' is used.
    
    - performance - prioritize cool components by having a higher fan speed.  Fan should be on almost all the time and scale up agressively with CPU.
      - For: users who want to ensure their components are cool (which may increase their lifespan) at the expense of fan noise.
      - fan max speed: 255 (100%)
      - fan min temp: 30000
      - fan max temp: 65000
    
    - quiet - prioritize lower fan noise/speed at the expense of hotter device temperatures (and possibly shorter component life span).  Fan will start during moderately intensive gaming and only scale up to 50% speed.  NOTE: 'critical' temp will still put fan to 100% which should avoid hitting the max operating temperature of 85 degrees C
      - For: users who don't like fan noise and are willing to take some risk of shorter component lifespan due to hotter components.
      - fan max speed: 127 (~50%)
      - fan min temp: 45000
      - fam max temp: 75000
      - fan critical temp: 75000
      - fan critical fan speed: 255 (100%)
    
    Across all profiles:
    - fan speed increases linearly from min to max.  A lower max/min mean high fan speeds at any given point.
    - fan min speed: 59
      - NOTE: this is changed from 55 as during testing,  values of <58 start fan, but it stops resulting in noise every ~10 seconds when restarting
    - critical temp: 75000
    - critical fan speed: 255

--- packages/351elec/sources/scripts/fan_control.sh
+++ packages/351elec/sources/scripts/fan_control.sh
@@ -7,30 +7,62 @@
 FAN=/sys/class/hwmon/hwmon0/pwm1
 CPU=/sys/class/hwmon/hwmon1/temp1_input
 GPU=/sys/class/hwmon/hwmon2/temp1_input
+CONFIG_FILE="/storage/.config/distribution/configs/distribution.conf"
 
-let TEMP_MIN=55000 #everything below this will turn off the fan
-let TEMP_MAX=65000 #everything above this will turn fan to max
-let TEMP_PREV=40000  # last temp measured (previous cicle)
-let TEMP_CURRENT=0  # current temp measured (current cicle)
+source /etc/profile
 
-let FAN_OFF=0 # fan off
-let FAN_MIN=55 # fan min speed
-let FAN_MAX=255 # fan max peed
-let FAN_PREV=125 # last fan speed (the first time we start from 50%)
-FIRST_TIME="true"
+function set_fan_variables() {
+  let FAN_SLEEP=15 # how long to sleep before reevaluating
+  let FAN_OFF=0 # fan off
+  let FAN_MIN=59 # fan min speed (below 59 the fan starts briefly and then stops, making more noise)
+  let FAN_MAX=255 # fan max speed
+  let TEMP_MIN=40000 #everything below this will turn off the fan
+  let TEMP_MAX=70000 #everything above this will turn fan to max
 
+  # 'critical' temp is mostly to allow for 'quiet' to still hit 255 without being 'linear'
+  #  at some point without gradually scaling up to it since 'MAX' will be less than 255
+  let FAN_CRITICAL=255 # fan speed at critical temp
+  let TEMP_CRITICAL=75000
 
+  if [[ "$FAN_PROFILE" == "performance" ]]; then
+    let TEMP_MIN=30000
+    let TEMP_MAX=65000
+  elif [[ "$FAN_PROFILE" == "quiet" ]]; then
+    let TEMP_MIN=45000
+    let TEMP_MAX=$TEMP_CRITICAL
+    let FAN_MAX=127
+  fi
+  echo "Fan profile: ${FAN_PROFILE}"
+  echo "TEMP_MIN: ${TEMP_MIN} TEMP_MAX: ${TEMP_MAX} FAN_MIN: ${FAN_MIN} FAN_MAX: ${FAN_MAX} SLEEP: ${FAN_SLEEP}"
+  let TEMP_CURRENT=0  # current temp measured (current cycle)
+  # Constant to be used in the Linear funciton to increase the fan (to avoid division by number lower than zero we use the two expressions)
+  let FAN_K_DIFF=$FAN_MAX-$FAN_MIN;
+  let TEMP_K_DIFF=$TEMP_MAX-$TEMP_MIN;
 
-
-# Constant to be used in the Linear funciton to increase the fan (to avoid division by number lower than zero we use the two expressions)
-let FAN_K_DIFF=$FAN_MAX-$FAN_MIN;
-let TEMP_K_DIFF=$TEMP_MAX-$TEMP_MIN; 
-#let K=$FAN_K_DIFF/$TEMP_K_DIFF
-
+}
 
 
 while true; do
   
+  # To avoid calling get_ee_setting each time through loop, only check fan.profile setting when distribution.conf changes
+  CONFIG_FILE_LAST_MODIFIED_TIME=$(date -r ${CONFIG_FILE})
+  
+  if [[ "${CONFIG_FILE_LAST_MODIFIED_TIME}" != "${CONFIG_FILE_LAST_MODIFIED_TIME_PREV}" ]]; then
+    FAN_PROFILE=$(get_ee_setting fan.profile)
+    if [[ -z "$FAN_PROFILE" ]]; then
+      FAN_PROFILE=default
+    fi
+    # To avoid outputting logging as though profile changed, check fan.profile changed
+    if [[ "$FAN_PROFILE" != "$FAN_PROFILE_PREV" ]]; then
+      set_fan_variables
+    fi
+
+    # Variables to keep track of to see if fan.profile changed
+    CONFIG_FILE_LAST_MODIFIED_TIME_PREV=$(date -r ${CONFIG_FILE})
+    FAN_PROFILE_PREV="${FAN_PROFILE}"
+
+  fi
+  FAN_PROFILE_PREV="${FAN_PROFILE}"
   OUTPUT=""
   TEMP_CURRENT_CPU=`cat ${CPU}`
   TEMP_CURRENT=`cat ${CPU}`
@@ -43,43 +75,27 @@ while true; do
   fi
   OUTPUT+="-CPU: ${TEMP_CURRENT_CPU} -GPU: ${TEMP_CURRENT_GPU} -Max temp: ${TEMP_CURRENT}";
   
-  if [ "$FIRST_TIME" == "true" ]; then
-  # first time we se the fan at 50%
-    echo $FAN_PREV  > $FAN
-    OUTPUT+=" -- First run - Setting pwm to: ${FAN_PREV}"
-    FIRST_TIME="false"
-    # Set the fan speed based on highest temperature
-  elif [ "$TEMP_CURRENT" -le "$TEMP_MIN" ]; then
-    FAN_PREV=`cat $FAN` 
+  if [ "$TEMP_CURRENT" -le "$TEMP_MIN" ]; then
     # set fan to off
     echo $FAN_OFF  > $FAN
     OUTPUT+=" -- Reached min temp - Setting pwm to: ${FAN_OFF}"
+  elif [ "$TEMP_CURRENT" -ge "$TEMP_CRITICAL" ]; then
+    # set fan to full speed
+    echo $FAN_CRITICAL > $FAN
+    OUTPUT+=" -- Reached critical temp - Setting pwm to: ${FAN_CRITICAL}"
   elif [ "$TEMP_CURRENT" -ge "$TEMP_MAX" ]; then
-  FAN_PREV=`cat $FAN`
-  # set fan to full speed
-  echo $FAN_MAX > $FAN
-  OUTPUT+=" -- Reached max temp - Setting pwm to: ${FAN_MAX}"
+    # set fan to full speed
+    echo $FAN_MAX > $FAN
+    OUTPUT+=" -- Reached max temp - Setting pwm to: ${FAN_MAX}"
   else
     # Calculate linear fuction value between FAN_MIN and FAN_MAX
-
     let TEMP_DIFF=$TEMP_CURRENT-$TEMP_MIN
-   #echo ">>TEMP_CURRENT: "$TEMP_CURRENT
-   # echo ">>TEMP_PREV: "$TEMP_PREV
-   # echo ">>TEMP_DIFF: "$TEMP_DIFF
     let NEW_FAN=$FAN_MIN+$TEMP_DIFF*$FAN_K_DIFF/$TEMP_K_DIFF 
-   if  [ $TEMP_DIFF -eq 0 ] && [ "$FIRST_TIME" != "true" ] ; then
-      NEW_FAN=$FAN_PREV
-   fi
-   
-    #echo ">>NEW_FAN: "$NEW_FAN
     echo $NEW_FAN > $FAN
     OUTPUT+=" -- Adapting fan - Setting pwm to: ${NEW_FAN}"
   fi
-  
-  FAN_CURRENT=`cat $FAN`
-  TEMP_PREV=$TEMP_CURRENT
 
   echo "${OUTPUT}"
 
-  sleep 15;
+  sleep $FAN_SLEEP;
 done
