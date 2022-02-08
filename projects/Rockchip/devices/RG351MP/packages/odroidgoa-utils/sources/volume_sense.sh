f69bb67606d3ba53f49364d24682aa7856ae4fdb
pkegg <pkegg@users.noreply.github.com>
2022-02-08 12:03:06 -0700

    Fix volume control to be faster
    Summary: Some users have complained that volume control was a bit slow.
    
    I did a bit of research and compared to a Switch and IOS/Android it is indeed much slower.
    
    This change makes the overall much faster, while still allowing single % adjustments (helpful for users where sound range due to hardware differences isn't uniform across all 100%) and smaller adjustments if you don't hold very long.
    
    The volume adjustment is still slightly slower than other devices, but I feel it's a good compramise between existing behavior and other systems behavior.

--- projects/Rockchip/devices/RG351MP/packages/odroidgoa-utils/sources/volume_sense.sh
+++ projects/Rockchip/devices/RG351MP/packages/odroidgoa-utils/sources/volume_sense.sh
@@ -103,11 +103,13 @@ done
           fi
 
           INCREMENT_AMOUNT=1
-          if [[ "${REPEAT_NUM}" -gt "75" ]]; then
+          if [[ "${REPEAT_NUM}" -gt "10" ]]; then
+             INCREMENT_AMOUNT=10
+          elif [[ "${REPEAT_NUM}" -gt "6" ]]; then
              INCREMENT_AMOUNT=5
-          elif [[ "${REPEAT_NUM}" -gt "25" ]]; then
+          elif [[ "${REPEAT_NUM}" -gt "3" ]]; then
              INCREMENT_AMOUNT=2
-          fi   
+          fi
           # Run the commands to adjust volume/brightness
           if [[ "${line}" == ${VOL_UP} ]]; then
             ${COMMAND} ${UP} ${INCREMENT_AMOUNT} > /dev/null
