a8468ce53f1915e55f4b6dad11f97685c4bb92f6
dhwz <dhwz@gmx.net>
2022-02-14 00:48:02 +0100

    fix SDL-GameControllerDB

--- packages/games/emulators/PPSSPPSDL/ppsspp.sh
+++ packages/games/emulators/PPSSPPSDL/ppsspp.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
-cp -f /usr/config/SDL-GameControllerDB/gamecontrollerdb.txt /storage/.config/SDL-GameControllerDB/gamecontrollerdb.txt
+cp -rf /storage/.config/SDL-GameControllerDB/gamecontrollerdb.txt /storage/roms/gamedata/ppsspp/assets/gamecontrollerdb.txt
 
 ARG=${1//[\\]/}
 export SDL_AUDIODRIVER=alsa          
