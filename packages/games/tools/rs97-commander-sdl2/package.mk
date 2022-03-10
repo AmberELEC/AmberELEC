6535e4f5f22d6601d66f5f80b9724c5ee435399f
dhwz <dhwz@gmx.net>
2022-03-10 17:41:03 +0100

    add 552 support for filemanager

--- packages/games/tools/rs97-commander-sdl2/package.mk
+++ packages/games/tools/rs97-commander-sdl2/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="rs97-commander-sdl2"
-PKG_VERSION="7d0b939508602c1035283d9062d258d695ad1f0f"
+PKG_VERSION="51aac5e769a1d58bc015350156670a52280e7be5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/dhwz/rs97-commander-sdl2"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_image SDL2_gfx SDL2_ttf patchelf"
+PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_image SDL2_gfx SDL2_ttf"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="Two-pane commander for RetroFW and RG-350 (fork of Dingux Commander)"
@@ -20,9 +20,10 @@ pre_configure_target() {
   fi
   sed -i "s|sdl2-config|${SYSROOT_PREFIX}/usr/bin/sdl2-config|" Makefile
 
-  if [ "${DEVICE}" = "RG351V" ] || [ "${DEVICE}" = "RG351MP" ]
-  then
+  if [ "${DEVICE}" = "RG351V" ] || [ "${DEVICE}" = "RG351MP" ]; then
     PKG_MAKE_OPTS_TARGET=" RG351V=1 CC=$CXX"
+  elif [ "${DEVICE}" = "RG552" ]; then
+    PKG_MAKE_OPTS_TARGET=" RG552=1 CC=$CXX"
   else
     PKG_MAKE_OPTS_TARGET=" RG351P=1 CC=$CXX"
   fi
