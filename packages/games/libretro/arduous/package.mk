a2c3fbc2813f97013f00c434c442514f11660f2b
YonKuma <kumarei@gmail.com>
2022-03-19 16:13:40 -0400

    Added Arduboy emulator

---
+++ packages/games/libretro/arduous/package.mk
@@ -0,0 +1,25 @@
+PKG_NAME="arduous"
+PKG_VERSION="8b98b980b436580ad51b9479daf6e9ca206b273b"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/arduous"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="arduous for libretro"
+PKG_LONGDESC="arduous for libretro/"
+
+PKG_IS_ADDON="no"
+PKG_TOOLCHAIN="cmake-make"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  export CXXFLAGS="$CXXFLAGS -Wno-error=maybe-uninitialized"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp arduous_libretro.so $INSTALL/usr/lib/libretro/
+}
