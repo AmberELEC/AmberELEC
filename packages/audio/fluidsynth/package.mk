30819d08662c39b7dff467e550af6fd765f69ec9
dhwz <dhwz@gmx.net>
2022-04-13 16:46:09 +0200

    bump fluidsynth

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -1,19 +1,34 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="1.1.6"
-PKG_SHA256="d28b47dfbf7f8e426902ae7fa2981d821fbf84f41da9e1b85be933d2d748f601"
-PKG_LICENSE="GPL"
+PKG_VERSION="2.2.6"
+#PKG_SHA256="ca90fe675cacd9a7b442662783c4e7fa0e1fd638b28d64105a4e3fe0f618d20f"
+PKG_LICENSE="LGPL"
 PKG_SITE="http://fluidsynth.org/"
-PKG_URL="$SOURCEFORGE_SRC/project/fluidsynth/fluidsynth-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain glib"
-PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
+PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain glib soundfont-generaluser"
+PKG_LONGDESC="FluidSynth is a software real-time synthesizer based on the Soundfont 2 specifications."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
-                       -DLIB_SUFFIX= \
-                       -Denable-readline=0 \
-                       -Denable-pulseaudio=0 \
-                       -Denable-libsndfile=0"
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DLIB_SUFFIX= \
+                         -Denable-readline=0 \
+                         -Denable-oss=0 \
+                         -Denable-pulseaudio=1 \
+                         -Denable-libsndfile=1"
+}
+
+post_makeinstall_target() {
+  # Create directories
+  mkdir -p ${INSTALL}/etc/fluidsynth
+  mkdir -p ${INSTALL}/usr/config/fluidsynth/soundfonts
+
+  # Create symlinks & install config file
+  cp -a ${PKG_DIR}/config/* ${INSTALL}/usr/config/fluidsynth/
+  ln -s /storage/.config/fluidsynth/fluidsynth.conf ${INSTALL}/etc/fluidsynth/
+  echo "Place your SoundFonts here!" >> ${INSTALL}/usr/config/fluidsynth/soundfonts/readme.txt
+
+  # Create symlink to SoundFont
+  ln -s /usr/share/soundfonts/GeneralUser.sf2  ${INSTALL}/usr/config/fluidsynth/soundfonts/
+}
