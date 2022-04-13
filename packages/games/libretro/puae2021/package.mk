5be3321a2291bcf7b5f379a3a34e8716f412ddca
dhwz <dhwz@gmx.net>
2022-04-13 16:06:56 +0200

    add puae2021

--- packages/games/libretro/puae2021/package.mk
+++ packages/games/libretro/puae2021/package.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2020-present AmberELEC (https://github.com/AmberELEC)
+# Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
-PKG_NAME="puae"
-PKG_VERSION="7618788c387e79b79a86b8d97bdaaf80ccc13824"
-PKG_SHA256="df99d2f8e9be3689ba05128cb5432962f812e806419d8dc8c3014f94c0475c50"
+PKG_NAME="puae2021"
+PKG_VERSION="2607dee0f48861c1982eb12d4d8538d594110962"
+PKG_SHA256="f7dc11cc7251c33875a4ebff63f1810cdbce819ec69785cea3a7d481b5aca555"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -16,8 +16,9 @@ PKG_SECTION="libretro"
 PKG_SHORTDESC="WIP libretro port of UAE (P-UAE and libco) Expect bugs"
 PKG_LONGDESC="WIP libretro port of UAE (P-UAE and libco) Expect bugs"
 PKG_TOOLCHAIN="make"
+PKG_GIT_CLONE_BRANCH="2.6.1"
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp puae_libretro.so $INSTALL/usr/lib/libretro/
+  cp puae2021_libretro.so $INSTALL/usr/lib/libretro/
 }
