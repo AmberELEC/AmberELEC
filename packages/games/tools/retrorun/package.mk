7f2edb709252cc512870afc94d01e4529cbbe156
dhwz <dhwz@gmx.net>
2022-04-17 20:34:35 +0200

    bump retrorun

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retrorun"
-PKG_VERSION="4204b79720141948f6fcc1eb92106b499aa445ac"
+PKG_VERSION="7e3ab628d465a024a06ee113de50929b873157d0"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/AmberELEC/retrorun-go2"
