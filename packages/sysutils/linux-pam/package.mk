1fe8e2f1f024ab83fa63db8844d271fa2e7df879
pkegg <pkegg@users.noreply.github.com>
2022-04-20 17:18:38 -0600

    Include initial limited version of libpam
    This includes libpam.so and other parts of pam into the image.  However,
    it does **not** integrate it with busybox or anything else.  That can
    come later if desired.
    
    This change includes enough of pam to allow python-pam to login (and
    likely other integrations to work).
    
    Additionally, the /etc/pam.d rules are very limited.  Again, this can be
    coordinated/done in the future.
    
    Inspired by:
    https://github.com/LibreELEC/LibreELEC.tv/tree/221dceb8b7203109c3ea7c12ed4212f3822911d0/packages/security/Linux-PAM
    (really old version of linux-pam in LibreELEC) and https://github.com/maximeh/buildroot/blob/master/package/linux-pam/linux-pam.mk (buildroot version of linux-pam)

---
+++ packages/sysutils/linux-pam/package.mk
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present AmberELEC team
+PKG_NAME="linux-pam"
+PKG_VERSION="1.5.2"
+PKG_SHA256="e4ec7131a91da44512574268f493c6d8ca105c87091691b8e9b56ca685d4f94d"
+PKG_URL="https://github.com/linux-pam/linux-pam/releases/download/v${PKG_VERSION}/Linux-PAM-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="autotools"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-prelude \
+	--disable-isadir \
+  --disable-selinux \
+  --disable-cracklib \
+  --disable-audit \
+	--disable-nis \
+	--disable-db \
+	--disable-regenerate-docu \
+	--enable-securedir=/lib/security \
+	--libdir=/lib"
+
+
+makeinstall_target() {
+  mkdir -p $INSTALL/etc/pam.d
+  cp $PKG_DIR/pam.d/* $INSTALL/etc/pam.d
+  
+  mkdir -p $INSTALL/usr/lib
+  cp -P $PKG_BUILD/.${TARGET_NAME}/libpam/.libs/*.so* $INSTALL/usr/lib
+  cp -P $PKG_BUILD/.${TARGET_NAME}/libpam_misc/.libs/*.so* $INSTALL/usr/lib
+  rm -rf $INSTALL/usr/lib/libpam_misc.so*T
+  cp -P $PKG_BUILD/.${TARGET_NAME}/libpamc/.libs/*.so* $INSTALL/usr/lib
+
+  mkdir -p $INSTALL/usr/lib/security
+  cp -P $PKG_BUILD/.${TARGET_NAME}/modules/*/.libs/*.so $INSTALL/usr/lib/security
+
+  mkdir -p $INSTALL/usr/lib/security/pam_filter
+  cp -P $PKG_BUILD/.${TARGET_NAME}/modules/pam_filter/upperLOWER/.libs/upperLOWER $INSTALL/usr/lib/security/pam_filter
+}
+
+
