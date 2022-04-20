81782c145b76b635bc5c9ae3c3912bd7b9fe42a5
YonKuma <kumarei@gmail.com>
2022-04-20 06:41:40 -0400

    Added evdev-joystick

--- packages/amberelec/package.mk
+++ packages/amberelec/package.mk
@@ -18,7 +18,7 @@ PKG_TOOLCHAIN="make"
 
 PKG_EXPERIMENTAL=""
 PKG_EMUS="$LIBRETRO_CORES advancemame PPSSPPSDL amiberry hatarisa openbor scummvmsa solarus hypseus-singe ecwolf lzdoom gzdoom raze drastic duckstation mupen64plussa piemu yabasanshiroSA"
-PKG_TOOLS="grep wget ffmpeg libjpeg-turbo common-shaders glsl-shaders MC util-linux xmlstarlet sixaxis jslisten evtest mpv bluetool rs97-commander-sdl2 jslisten gnupg gzip patchelf valgrind strace gdb apitrace rg351p-js2xbox gptokeyb odroidgoa-utils rs97-commander-sdl2 textviewer 351files rclone jstest-sdl sdljoytest"
+PKG_TOOLS="grep wget ffmpeg libjpeg-turbo common-shaders glsl-shaders MC util-linux xmlstarlet sixaxis jslisten evtest mpv bluetool rs97-commander-sdl2 jslisten gnupg gzip patchelf valgrind strace gdb apitrace rg351p-js2xbox gptokeyb odroidgoa-utils rs97-commander-sdl2 textviewer 351files rclone jstest-sdl sdljoytest evdev-joystick"
 PKG_RETROPIE_DEP="bash pyudev dialog six git dbus-python coreutils"
 PKG_DEPENDS_TARGET+=" $PKG_TOOLS $PKG_RETROPIE_DEP $PKG_EMUS $PKG_EXPERIMENTAL ports"
 
---
+++ packages/games/tools/evdev-joystick/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2022-present Team AmberELEC (https://amberelec.org)
+
+PKG_NAME="evdev-joystick"
+PKG_VERSION="0"
+PKG_LICENSE="GPL2"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="stella"
+PKG_SHORTDESC="evdev-joystick binary from Stella"
+PKG_LONGDESC="evdev-joystick is used to set the deadzone for Linux 'evdev' joystick devices."
+PKG_TOOLCHAIN="make"
+
+pre_configure_target() {
+  cd "$(get_build_dir stella)/src/tools/evdev-joystick"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+  cp evdev-joystick $INSTALL/usr/bin
+}
