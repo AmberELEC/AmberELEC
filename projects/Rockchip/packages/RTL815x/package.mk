e4aed2299bb1888a1aafc7c375df27f4b07ba4a5
dhwz <dhwz@gmx.net>
2022-04-20 23:05:48 +0200

    update drivers

---
+++ projects/Rockchip/packages/RTL815x/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
+
+PKG_NAME="RTL815x"
+PKG_VERSION="54b8e299e15944e3c023a0eb955eab8416b13d2a"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/dhwz/realtek-r8152-linux"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="Realtek RTL8152/RTL8153/RTL8156 Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+make_target() {
+  LDFLAGS="" make -C $(kernel_path) M=$PKG_BUILD \
+    ARCH=$TARGET_KERNEL_ARCH \
+    KSRC=$(kernel_path) \
+    CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+    USER_EXTRA_CFLAGS="-fgnu89-inline"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+  find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+}
