0927218cd0edbad197ac7c708ce3222e40c89b1d
KK <42102568+kkoshelev@users.noreply.github.com>
2022-05-05 16:17:50 -0700

    Revert wrong branch overlap

--- packages/amberelec/sources/scripts/mpv_video.sh
+++ packages/amberelec/sources/scripts/mpv_video.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if [ "${1: -4}" == ".mid" ]
-then
-  PORT=$(aplaymidi -l | awk '/.*([0-9]*:[0-9]*) .*/ {a=$1} END{print a}')
-  echo Playing "${1}" on midi port $PORT
-  /usr/bin/aplaymidi -p $PORT "${1}"
-  exit 0
-fi
-
 /usr/bin/mpv --input-ipc-server=/tmp/mpvsocket "${1}"
 exit 0
