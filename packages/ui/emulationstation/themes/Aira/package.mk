2782f300394ddac3280845d4dc743ba2e5e1fb66
Szalik <77732736+SzalikDesigns@users.noreply.github.com>
2022-05-07 14:21:28 +0200

    Theme Updates
    - Update art-book-next theme
    - Switch Default theme for something more "ours" but keep art-book-next downloaded
    - Remove fullscreen background lines, as its no longer exist here

--- packages/ui/emulationstation/themes/Aira/package.mk
+++ packages/ui/emulationstation/themes/Aira/package.mk
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2020 Shanti Gilbert (https://github.com/shantigilbert)
-# Copyright (C) 2021 AmberELEC (https://github.com/AmberELEC)
+# Copyright (C) 2022 AmberELEC (https://github.com/AmberELEC)
 
-PKG_NAME="es-theme-art-book-next"
+PKG_NAME="Aira"
 PKG_REV="1"
-PKG_VERSION="e5943ba1009d954d24f4a3c711a891c7c2f57c6d"
+PKG_VERSION="992c68e3729c52b6159a92aae187854b340e0c0b"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
-PKG_SITE="https://github.com/anthonycaccese/es-theme-art-book-next"
+PKG_SITE="https://github.com/SzalikDesigns/Aira"
 PKG_URL="$PKG_SITE.git"
-PKG_SHORTDESC="ArtBook Next"
-PKG_LONGDESC="Art Book Next - AmberELEC default theme"
+PKG_SHORTDESC="Aira"
+PKG_LONGDESC="Aira - AmberELEC default theme"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
@@ -25,6 +25,11 @@ makeinstall_target() {
     elif [[ "$DEVICE" == "RG351V" || "$DEVICE" == "RG351MP" ]]; then
       RESOLUTION="640x480"
     fi
-
+    # Cleanup the directories of images not needed on device.  For non-552 devices, this saves ~30MB
+    if [[ "$RESOLUTION" != "" ]]; then
+      pushd $INSTALL/usr/config/emulationstation/themes/$PKG_NAME/_inc/images/systems/fullscreen || exit 1
+      find . -type d -not -name "$RESOLUTION" -not -name '.' -print0 | xargs -0 -I {} rm -rf '{}'
+      popd
+    fi
 
 }
