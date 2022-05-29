d62d082c7af7e9db812ba7ef5d555c8afa1deaf3
Tiago Medeiros <madcat@altocircuito.net>
2022-05-29 04:18:09 -0400

    Make Drastic configurable through emulation station

--- packages/games/emulators/drastic/drastic.sh
+++ packages/games/emulators/drastic/drastic.sh
@@ -4,22 +4,78 @@ source /etc/profile
 
 EE_DEVICE=$(cat /storage/.config/.OS_ARCH)
 EXECLOG="/tmp/logs/exec.log"
+ROM="${1##*/}"
+CORE="drastic"
+PLATFORM="nds"
+BASEFOLDER="/storage/drastic/aarch64/drastic/"
+CONFFOLDER="${BASEFOLDER}config"
+DRASTICFOLDER="${BASEFOLDER}drastic"
+CONFFILE="${CONFFOLDER}/drastic.cfg"
+CONF="/storage/.config/distribution/configs/distribution.conf"
 
-if [ ! -d "/storage/drastic/aarch64/drastic/config" ]
+function get_setting() {
+        #We look for the setting on the ROM first, if not found we search for platform and lastly we search globally
+        PAT="s|^${PLATFORM}\[\"${ROM}\"\].*${1}=\(.*\)|\1|p"
+        EES=$(sed -n "${PAT}" "${CONF}" | head -1)
+
+        if [ -z "${EES}" ]; then
+                PAT="s|^${PLATFORM}[\.-]${1}=\(.*\)|\1|p"
+                EES=$(sed -n "${PAT}" "${CONF}" | head -1)
+        fi
+
+        if [ -z "${EES}" ]; then
+                PAT="s|^global[\.-].*${1}=\(.*\)|\1|p"
+                EES=$(sed -n "${PAT}" "${CONF}" | head -1)
+        fi
+
+        [ -z "${EES}" ] && EES="false"
+}
+
+function set_config_value(){
+        sed -i "s/^\(${1} = \).*/${1} = ${2}/" ${CONFFILE}
+}
+
+
+
+if [ ! -d "${CONFFOLDER}" ]
 then
-  mkdir -p /storage/drastic/aarch64/drastic/config
-  cp -rf /usr/share/drastic/drastic.cfg /storage/drastic/aarch64/drastic/config
+  mkdir -p "${CONFFOLDER}"
+  cp -rf /usr/share/drastic/drastic.cfg "${CONFFOLDER}"
 fi
-if [ ! -f "/storage/drastic/aarch64/drastic/config/drastic.cfg" ]
+if [ ! -f "${CONFFILE}" ]
 then
-  cp -rf /usr/share/drastic/config/drastic.cfg /storage/drastic/aarch64/drastic/config
+  cp -rf /usr/share/drastic/config/drastic.cfg "${CONFFOLDER}"
 fi
 
-if [ ! -f "/storage/drastic/aarch64/drastic/drastic" ]
+if [ ! -f "${DRASTICFOLDER}" ]
 then
-  mkdir -p /storage/drastic/aarch64/drastic/
-  cp -rf /usr/share/drastic/data/* /storage/drastic/aarch64/drastic/
+  mkdir -p "${BASEFOLDER}"
+  cp -rf /usr/share/drastic/data/* "${BASEFOLDER}"
 fi
 
-cd /storage/drastic/aarch64/drastic/
-./drastic "$1" >> $EXECLOG 2>&1
+cd "${BASEFOLDER}"
+
+
+
+# EmulationStation settings implementation
+get_setting "highres_3d"
+if [ "${EES}" != "false" ]; then
+	set_config_value "hires_3d" "${EES}"
+fi
+
+get_setting "threaded_3d"
+if [ "${EES}" != "false" ]; then
+	set_config_value "threaded_3d" "${EES}"
+fi
+
+get_setting "frameskip_type"
+if [ "${EES}" != "false" ]; then
+	set_config_value "frameskip_type" "${EES}"
+fi
+
+get_setting "frames_skipped"
+if [ "${EES}" != "false" ]; then
+	set_config_value "frameskip_value" "${EES}"
+fi
+
+./drastic "${1}" >> $EXECLOG 2>&1
