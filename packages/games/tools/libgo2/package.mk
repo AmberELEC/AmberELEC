ca8dacccd3b18af9b6b40a14020de33d182bdb34
navy1978 <navy1978@gmail.com>
2022-07-21 15:38:23 +0000

    test libgo2

--- packages/games/tools/libgo2/package.mk
+++ packages/games/tools/libgo2/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="libgo2"
-PKG_VERSION="c01d746babf85a52e6c6996861e98916c72d7846"
+PKG_VERSION="6f5de533cdac447d34410c106b05071527114463"
 PKG_SHA256="89ad1cf229d581fa8e4498f4f4c526215176e79885e935bd7dc48c5872655f92"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="LGPL"
