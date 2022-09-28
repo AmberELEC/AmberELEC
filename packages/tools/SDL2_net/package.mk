943f0dceb37ce57ee8ad543305c35144fd11b3d8
Tiago Medeiros <madcat@altocircuito.net>
2022-09-27 21:22:26 -0400

    Updated SDL2 packages

--- packages/tools/SDL2_net/package.mk
+++ packages/tools/SDL2_net/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL2_net"
-PKG_VERSION="2.0.1"
-PKG_SHA256="15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21"
+PKG_VERSION="2.2.0"
+PKG_SHA256="4e4a891988316271974ff4e9585ed1ef729a123d22c08bd473129179dc857feb"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/projects/SDL_net/"
 PKG_URL="https://www.libsdl.org/projects/SDL_net/release/$PKG_NAME-$PKG_VERSION.tar.gz"
