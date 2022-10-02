bf30b375a18def79d6313c3fbd99c4ed84f0f725
Tiago Roque Medeiros <madcat@altocircuito.net>
2022-10-02 09:20:40 -0400

    bump SDL2 / fix mouse on portmaster / cleanups

--- packages/tools/SDL2_mixer/package.mk
+++ packages/tools/SDL2_mixer/package.mk
@@ -2,14 +2,17 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="SDL2_mixer"
-PKG_VERSION="2.0.4"
+PKG_VERSION="2.6.2"
 PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 libogg libvorbis flac mpg123 libmodplug"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 libogg libvorbis flac mpg123 libmodplug fluidsynth"
 PKG_LONGDESC="SDL2 mixer"
 
 pre_configure_target() {
+  SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config
   PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET} --enable-music-mp3"
+  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_OPUS=OFF"
+
 }
