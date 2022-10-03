2e78610e4520c14f62e06c264e6840b7acf7a4a8
dhwz <dhwz@gmx.net>
2022-10-03 15:40:19 +0200

    bump retroarch to 1.11.0

--- packages/games/tools/retroarch/package.mk
+++ packages/games/tools/retroarch/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retroarch"
-PKG_VERSION="5ed375b7dfb0c1df3dcf4935c4622bcae3518061"
+PKG_VERSION="9586d679a8b143fbe1880dff7454fd0809262b55"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="$PKG_SITE.git"
 PKG_LICENSE="GPLv3"
