796bb35a0ea6cd30e0167ceb0e8363921edaa6d1
Tiago Roque Medeiros <madcat@altocircuito.net>
2022-11-13 14:01:58 -0500

    bump XZ

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xz"
-PKG_VERSION="5.2.5"
-PKG_SHA256="5117f930900b341493827d63aa910ff5e011e0b994197c3b71c08a20228a42df"
+PKG_VERSION="5.2.7"
+PKG_SHA256="8712e9acb0b6b49a97d443458a3067dc5c08a025e02dc5f773176c51dd7cfc69"
 PKG_LICENSE="GPL"
 PKG_SITE="http://tukaani.org/xz/"
 PKG_URL="http://tukaani.org/xz/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
@@ -20,9 +20,11 @@ PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static \
                          --disable-lzmainfo \
                          --enable-lzma-links \
                          --disable-scripts \
-                         --disable-nls"
+                         --disable-nls \
+                         --enable-symbol-versions=no"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static \
+                           --enable-symbol-versions=no"
 
 post_makeinstall_target() {
   rm -rf ${INSTALL}
