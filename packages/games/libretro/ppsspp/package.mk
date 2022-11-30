722d4daf13f552152beec35d45806bcbc77de6a0
Tiago Medeiros <medeiros1990@yahoo.com>
2022-11-30 16:37:01 -0500

    bump RA PPSSPP

--- packages/games/libretro/ppsspp/package.mk
+++ packages/games/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="41f344398844b4596e48de1122d443c621a39864"
+PKG_VERSION="f4385e1bea76961d0d27a5764d4d05e8ac208d6a"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
