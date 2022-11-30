f1f88f63b8b65f015dfbc4db2978c7dae4946016
Tiago Medeiros <medeiros1990@yahoo.com>
2022-11-30 16:35:40 -0500

    Updated MPV and WAF

--- packages/multimedia/mpv/package.mk
+++ packages/multimedia/mpv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mpv"
-PKG_VERSION="70b991749df389bcc0a4e145b5687233a03b4ed7"
-PKG_SHA256="417b45c2ac88b9e020b55adf9238a4b46dc8c80d4c1a3efe301d55aaae4b0b99"
+PKG_VERSION="7eb8f81091008d4e90dfc758df27342a541327f3"
+PKG_SHA256="5ecaa44664326b84583415afe88bfa92c15d16a50a34c92f20c4f7b5261cc64f"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/mpv-player/mpv"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
@@ -14,8 +14,8 @@ PKG_TOOLCHAIN="manual"
 configure_target() {
   #./bootstrap.py 
   # the bootstrap was failing for some reason. 
-  cp $PKG_DIR/waf/* $PKG_BUILD  
-  
+  cp ${PKG_DIR}/waf/* ${PKG_BUILD}  
+  cd ${PKG_BUILD}
   ./waf configure --enable-sdl2 --enable-sdl2-gamepad --disable-pulse --enable-egl --disable-libbluray --disable-gl
 }
 
@@ -24,6 +24,6 @@ make_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-  cp ./build/mpv $INSTALL/usr/bin
+  mkdir -p ${INSTALL}/usr/bin
+  cp ./build/mpv ${INSTALL}/usr/bin
 }
