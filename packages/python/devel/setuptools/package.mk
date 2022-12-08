b8c0922b8cf7b4480af48141ac86f3ff5baf5e52
dhwz <dhwz@gmx.net>
2022-12-08 15:50:06 +0100

    fix

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="setuptools"
-PKG_VERSION="52.0.0"
-PKG_SHA256="ff0c74d1b905a224d647f99c6135eacbec2620219992186b81aa20012bc7f882"
+PKG_VERSION="51.3.3" # don't change this, this will break webui install
+PKG_SHA256="8e45c6cb18f81842421560f788521842572a91a0e64419e338a6a15828ccf076"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
 PKG_URL="https://github.com/pypa/setuptools/archive/v${PKG_VERSION}.tar.gz"
