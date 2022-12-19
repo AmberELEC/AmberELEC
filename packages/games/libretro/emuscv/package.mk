f84ba259ad1f6d0881abe3f6e19f9d3113ffdc88
dhwz <dhwz@gmx.net>
2022-12-19 20:45:51 +0100

    add emuscv core

---
+++ packages/games/libretro/emuscv/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
+
+PKG_NAME="emuscv"
+PKG_VERSION="dfce10df090ce3f5eb23bdbee289702ec1478246"
+#PKG_ARCH="aarch64"
+PKG_SITE="https://gitlab.com/MaaaX-EmuSCV/libretro-emuscv"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="An EPOCH/YENO Super Cassette Vision (1984) home video game emulator for Libretro"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C . platform=unix"
+
+pre_configure_target() {
+  export TERM=xterm
+  CXXFLAGS+=" -I$(get_build_dir glibc)/sysdeps/unix/sysv/linux/x86"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/emuscv_libretro.info $INSTALL/usr/lib/libretro/
+  cp $PKG_BUILD/emuscv_libretro.so $INSTALL/usr/lib/libretro/
+}
