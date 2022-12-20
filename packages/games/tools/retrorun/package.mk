66d80afa945b96a952bfbb2882ed3312803477dd
navy1978 <ivan.morelli@gmail.com>
2022-12-21 00:23:50 +0100

    bump retrorun

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retrorun"
-PKG_VERSION="efc6589875209cf323dc364c70f51b195e0ca5fd"
+PKG_VERSION="22e60ef28511156ed0f3e5e942af4d834a4f53c3"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/AmberELEC/retrorun-go2"
