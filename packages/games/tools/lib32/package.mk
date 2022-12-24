7c90b4ead154ef9e06edb2a64adc95131e4f2df3
dhwz <dhwz@gmx.net>
2022-12-24 12:23:29 +0100

    bump lib32

--- packages/games/tools/lib32/package.mk
+++ packages/games/tools/lib32/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="lib32"
-PKG_VERSION="384f27a38940532d228cee44a91211761facc856"
+PKG_VERSION="42f9d8d98e576edbc88499bf5bf5fa1bbe736101"
 PKG_ARCH="aarch64"
 PKG_SITE="https://github.com/AmberELEC/lib32"
 PKG_URL="$PKG_SITE.git"
