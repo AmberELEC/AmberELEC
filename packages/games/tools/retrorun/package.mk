aad3592bf1bbc016ec76aab2af765993b00295e4
navy1978 <ivan.morelli@gmail.com>
2023-01-04 16:27:36 +0000

    retrorun: test new version that includes libgo2

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retrorun"
-PKG_VERSION="119ffc2c2b921f98d07aec11acbc21bfa622d82e"
+PKG_VERSION="b29e4636d0323fdea32cb3dd3af61dcb4af47096"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun-go-removed"
