4ad259f7a821c8b1486cee7db139563cecc1397c
dhwz <dhwz@gmx.net>
2023-01-15 19:52:08 +0100

    add gptokeyb

---
+++ packages/amberelec/config/distribution/modules/SetTime.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# based on code from https://github.com/Rolen47/ChangeTime
+
+unset SDL_GAMECONTROLLERCONFIG_FILE
+source /usr/bin/env.sh
+export TERM=xterm-color
+export DIALOGRC=/etc/amberelec.dialogrc
+
+gptokeyb SetTime.sh -c /usr/config/gptokeyb/settime.gptk &
+
+clear > /dev/console
+
+userQuit() {
+ kill -9 $(pidof gptokeyb)
+ clear > /dev/console
+ exit 0
+}
+
+MainMenu() {
+ local dialog_options=( 1 "Set Time" 2 "Set Date" 3 "Quit" )
+
+ while true; do
+  current_time=`date +%H:%M:%S`
+  current_date=`date +%F`
+  current_zone=`date +%Z`
+  show_dialog=(dialog \
+  --title " Main menu " \
+  --clear \
+  --no-cancel \
+  --menu "$current_date $current_time $current_zone" 0 0 1)
+
+  choices=$("${show_dialog[@]}" "${dialog_options[@]}" 2>&1 > /dev/console) || userQuit
+
+  for choice in $choices; do
+    case $choice in
+    1) SetTime ;;
+    2) SetDate ;;
+    3) userQuit ;;
+   esac
+  done
+ done
+}
+
+SetTime() {
+ show_dialog=(dialog --title " Set Time " --timebox "Use Left/Right/Y to select the field, use Up/Down to change the value." 5 35)
+ desired_time=$("${show_dialog[@]}" 2>&1 > /dev/console) || MainMenu
+ if [ "$desired_time" != "" ]; then
+  date +%T -s "$desired_time"
+  hwclock --systohc --utc
+ fi
+}
+
+SetDate() {
+ show_dialog=(dialog --date-format "%Y-%m-%d" --title " Set Date " --calendar "Use Y to switch between Month, Year and Day selection, use Up/Down/Left/Right to select the value." 0 0)
+ desired_date=$("${show_dialog[@]}" 2>&1 > /dev/console) || MainMenu
+ if [ "$desired_date" != "" ]; then
+  current_time=`date +%H:%M:%S`
+  date +%Y-%m-%d -s "$desired_date $current_time"
+  hwclock --systohc --utc
+ fi
+}
+
+MainMenu
+userQuit
