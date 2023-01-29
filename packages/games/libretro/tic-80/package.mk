72b37b8539adb830f7477c6c104398e9ea8e7397
dhwz <dhwz@gmx.net>
2023-01-29 14:21:11 +0100

    bump tic-80

--- packages/games/libretro/tic-80/package.mk
+++ packages/games/libretro/tic-80/package.mk
@@ -2,14 +2,24 @@
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="tic-80"
-PKG_VERSION="34fb54e42e9e66b650e20b9ea782fc08dd588bc9"
+PKG_VERSION="2a662a0ad38514210c313b518f7691aab227c06c"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/nesbox/TIC-80"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain tic-80:host"
 PKG_LONGDESC="TIC-80 is a fantasy computer for making, playing and sharing tiny games."
 PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="+pic"
+
+PKG_CMAKE_OPTS_HOST="-DBUILD_PLAYER=OFF \
+                     -DBUILD_SOKOL=OFF \
+                     -DBUILD_SDL=OFF \
+                     -DBUILD_DEMO_CARTS=OFF \
+                     -DBUILD_TOUCH_INPUT=OFF \
+                     -DBUILD_LIBRETRO=ON \
+                     -DCMAKE_BUILD_TYPE=Release \
+                     -DCMAKE_RULE_MESSAGES=OFF \
+                     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_PLAYER=OFF \
                        -DBUILD_SOKOL=OFF \
