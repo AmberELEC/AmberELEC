20526e984200413b091e8cc94837e155540b5a9c
dhwz <dhwz@gmx.net>
2023-02-06 16:57:41 +0100

    bump openbor

--- packages/games/emulators/openbor/package.mk
+++ packages/games/emulators/openbor/package.mk
@@ -3,20 +3,17 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="openbor"
-PKG_VERSION="bde5af0a22860b860b97d858bb27422b2e4564e6"
+PKG_VERSION="ab4e792755bc005c0d628a39326665632b2555c0"
 PKG_SITE="https://github.com/DCurrent/openbor"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain SDL2 libogg libvorbisidec libvpx libpng"
 PKG_LONGDESC="OpenBOR is the ultimate 2D side scrolling engine for beat em' ups, shooters, and more!"
 PKG_TOOLCHAIN="make"
-PKG_GIT_CLONE_BRANCH="global_beta"
+PKG_GIT_CLONE_BRANCH="globa_beta_b"
 
 pre_configure_target() {
-  PKG_MAKE_OPTS_TARGET="BUILD_LINUX_${ARCH}=1 -C ${PKG_BUILD}/engine SDKPATH=${SYSROOT_PREFIX} PREFIX=${TARGET_NAME}"
+  PKG_MAKE_OPTS_TARGET="BUILD_LINUX_${ARCH}=1 NO_STRIP=1 -C ${PKG_BUILD}/engine SDKPATH=${SYSROOT_PREFIX} PREFIX=${TARGET_NAME}"
   cd ${PKG_BUILD}
-  #sed -i "s|device->mappings\[SDID_START\]      = SDL_CONTROLLER_BUTTON_START;|device->mappings\[SDID_START\]      = SDL_CONTROLLER_BUTTON_BACK;|g" engine/sdl/control.c
-  #sed -i "s|device->mappings\[SDID_SCREENSHOT\] = SDL_CONTROLLER_BUTTON_BACK;|device->mappings\[SDID_SCREENSHOT\] = SDL_CONTROLLER_BUTTON_START;|g" engine/sdl/control.c
-  sed -i "s|-Werror||g" engine/Makefile
   sed -i 's/\-O[23]//' engine/Makefile
 }
 
