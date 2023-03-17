8f7e25d585f4667dff0291bdbb12b56a27af20d0
dhwz <dhwz@gmx.net>
2023-03-17 12:14:09 +0100

    enable retroachievements for atomiswave/naomi

--- packages/amberelec/sources/scripts/setsettings.py
+++ packages/amberelec/sources/scripts/setsettings.py
@@ -193,11 +193,11 @@ def set_settings(rom_name: str, core: str, platform: str, controllers: str, auto
 
     # Retroachievements / Cheevos
     retro_achievements = {
-        'arcade', 'arduboy', 'atari2600', 'atari7800', 'atarilynx', 'channelf', 'colecovision',
-        'dreamcast', 'famicom', 'fbn', 'fds', 'gamegear', 'gb', 'gba', 'gbah', 'gbc', 'gbch',
-        'gbh', 'genesis', 'genh', 'ggh', 'intellivision', 'mastersystem',
+        'arcade', 'arduboy', 'atari2600', 'atari7800', 'atarilynx', 'atomiswave', 'channelf',
+        'colecovision', 'dreamcast', 'famicom', 'fbn', 'fds', 'gamegear', 'gb', 'gba', 'gbah',
+        'gbc', 'gbch', 'gbh', 'genesis', 'genh', 'ggh', 'intellivision', 'mastersystem',
         'megacd', 'megadrive', 'megadrive-japan', 'megaduck', 'msx', 'msx2', 'n64',
-        'neogeo', 'neogeocd', 'nes', 'nesh', 'ngp', 'ngpc', 'odyssey2',
+        'naomi', 'neogeo', 'neogeocd', 'nes', 'nesh', 'ngp', 'ngpc', 'odyssey2',
         'pcengine', 'pcenginecd', 'pcfx', 'pokemini', 'psp', 'psx', 'saturn', 'sega32x',
         'segacd', 'sfc', 'sg-1000', 'snes', 'snesh', 'snesmsu1', 'supergrafx',
         'supervision', 'tg16', 'tg16cd', 'vectrex', 'virtualboy', 'wonderswan',
