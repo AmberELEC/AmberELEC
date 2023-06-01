65ae1f40bfe178832f3979f79718a646a9f36548
dhwz <dhwz@gmx.net>
2023-06-01 18:44:11 +0200

    add uw8 core

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="1dacc9acc6e513e55f88d942d809a6a83c508d44"
+PKG_VERSION="bbca44a9f1ef79ee92b115ab45af808aca0e4703"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AmberELEC/emulationstation"
