8d41b6b4b210fa1b4c65d12444df2fcd6f47d144
navy1978 <ivan.morelli@gmail.com>
2023-06-09 09:30:07 +0000

    testing emulationstation patched for retrorun

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="5af6260147b47b56d865273ddc174fc97013c4bd"
+PKG_VERSION="fdc141a1c52b2fcbc299045ff0d0f88629b8b1ef"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/navy1978/emulationstation"
