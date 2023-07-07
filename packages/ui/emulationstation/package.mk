71a7e4b578570f80a8c8e6d8f2ae95054c838d3b
navy1978 <ivan.morelli@gmail.com>
2023-07-07 12:53:25 +0000

    bump emulationstation

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="6b92ce897836a09ba00bd68a49e7cb43979e9098"
+PKG_VERSION="49e4e1f5a809ee6ae2438fbd949b159c36a14eca"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/navy1978/emulationstation"
