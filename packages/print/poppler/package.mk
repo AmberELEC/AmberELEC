8e6e3644998898ced2e37d112315a2641653fadb
Tiago Roque Medeiros <madcat@altocircuito.net>
2023-10-07 14:21:18 -0400

    bump poppler

--- packages/print/poppler/package.mk
+++ packages/print/poppler/package.mk
@@ -4,15 +4,19 @@
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC
 
 PKG_NAME="poppler"
-PKG_VERSION="065dca3816db3979dfacdc2f8592abed2ff6859a"
+PKG_VERSION="23.10.0"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/freedesktop/poppler"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo boost"
+PKG_SITE="https://poppler.freedesktop.org/"
+PKG_URL="${PKG_SITE}poppler-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo boost tiff"
 PKG_LONGDESC="The poppler pdf rendering library "
 PKG_TOOLCHAIN="cmake-make"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_LIBOPENJPEG=none \
                        -DENABLE_GLIB=ON \
                        -DENABLE_QT5=OFF \
+                       -DENABLE_QT6=OFF \
+                       -DENABLE_NSS3=OFF \
+                       -DENABLE_LCMS=OFF \
+                       -DENABLE_GPGME=OFF \
                        -DENABLE_CPP=OFF"
