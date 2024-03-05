9371ddce7b2949e18e64e763f3e24b91301a15da
Hack Around <tech4bot@users.noreply.github.com>
2024-03-05 22:46:24 +0100

    autodetect device and add RAM info

--- packages/amberelec/sources/autostart.sh
+++ packages/amberelec/sources/autostart.sh
@@ -229,6 +229,21 @@ then
   /usr/bin/batocera-internal-wifi disable-no-refresh
 fi
 
+if [ "$(cat /sys/firmware/devicetree/base/model)" == "Anbernic RG351MP" ]; then
+  VOLT1=$(cat /sys/bus/iio/devices/iio:device0/in_voltage1_raw)
+  VOLT2=$(cat /sys/bus/iio/devices/iio:device0/in_voltage2_raw)
+  if (( ${VOLT2} < 500 )); then
+    if ((${VOLT1} >= 490 && ${VOLT1} <= 500)); then
+      echo "R36S" > /storage/.config/device
+    elif ((${VOLT1} >= 510 && ${VOLT1} <= 535)); then
+      echo "RGB20S" > /storage/.config/device
+    elif ((${VOLT1} >= 550 && ${VOLT1} <= 588)); then
+      echo "R35S" > /storage/.config/device
+    elif ((${VOLT1} >= 950 && ${VOLT1} <= 1035)); then
+      echo "R33S" > /storage/.config/device
+    fi
+  fi
+fi
 
 if [ "$(cat /sys/firmware/devicetree/base/model)" == "Anbernic RG351MP" ] || [ "$(cat /sys/firmware/devicetree/base/model)" == "PowKiddy Magicx XU10" ]; then
 	amixer -c 0 cset iface=MIXER,name='Playback Path' SPK_HP
