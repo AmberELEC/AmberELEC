db3bc5071e7f5a94a5834bbb7ed6d89433880875
Hack Around <tech4bot@users.noreply.github.com>
2024-03-19 21:37:20 +0100

    add d007 support

--- projects/Rockchip/devices/RG351MP/packages/odroidgoa-utils/package.mk
+++ projects/Rockchip/devices/RG351MP/packages/odroidgoa-utils/package.mk
@@ -15,15 +15,18 @@ PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
 	mkdir -p ${INSTALL}/usr/bin
-#	cp headphone_sense.sh ${INSTALL}/usr/bin
+	cp headphone_sense.sh ${INSTALL}/usr/bin
 	cp battery.sh ${INSTALL}/usr/bin
-        cp odroidgoa_utils.sh ${INSTALL}/usr/bin
+	cp odroidgoa_utils.sh ${INSTALL}/usr/bin
 	cp volume_sense.sh ${INSTALL}/usr/bin
-        chmod 0755 ${INSTALL}/usr/bin/*
+	cp adckeys.sh ${INSTALL}/usr/bin
+	cp adckeys.py ${INSTALL}/usr/bin
+	chmod 0755 ${INSTALL}/usr/bin/*
 }
 
 post_install() {  
 	enable_service volume.service
-#	enable_service headphones.service
+	enable_service headphones.service
 	enable_service battery.service
+	enable_service adckeys.service
 }
--- projects/Rockchip/packages/u-boot/package.mk
+++ projects/Rockchip/packages/u-boot/package.mk
@@ -15,7 +15,7 @@ PKG_NEED_UNPACK="${PROJECT_DIR}/${PROJECT}/bootloader"
 [ -n "${DEVICE}" ] && PKG_NEED_UNPACK+=" ${PROJECT_DIR}/${PROJECT}/devices/${DEVICE}/bootloader"
 
 if [[ "${DEVICE}" =~ RG351 ]]; then
-  PKG_VERSION="77ce61e705be4655679d661dbcc8996ef82b42f6"
+  PKG_VERSION="d8ad98256d4913bf39153a404f5e26e94cfe8b14"
   PKG_GIT_CLONE_SINGLE="yes"
   PKG_GIT_CLONE_DEPTH="1"
   PKG_URL="https://github.com/AmberELEC/uboot_rg351.git"
