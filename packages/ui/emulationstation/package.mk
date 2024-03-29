21731480412f1e0c7183c031185ef9c14516c3d2
dhwz <dhwz@gmx.net>
2024-03-29 20:22:21 +0100

    bump ES

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="67f34deb69b817f3fa757c079eec51b106fef6e7"
+PKG_VERSION="f4509dab54a5b0b0a06285d0541375e08aa2c5b9"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AmberELEC/emulationstation"
