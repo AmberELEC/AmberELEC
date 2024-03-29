0cdd108e6e8fd35201a77c26ec8de1f3ecb27c8a
dhwz <dhwz@gmx.net>
2024-03-29 20:24:28 +0100

    add new cores: geolith (Neo Geo AES/MVS), DoubleCherryGB (GB, GBC), theodore (Thomson MO/TO)

---
+++ packages/games/libretro/doublecherrygb/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present AmberELEC (https://github.com/AmberELEC)
+
+PKG_NAME="doublecherrygb"
+PKG_VERSION="abb98f44e416c06ee193506046664da340a641f8"
+PKG_SHA256="2604b1dc8c40ea7874aace5ea603fcff4f8f4c0252f597dc279470c1d8bc0a77"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/TimOelrichs/doublecherryGB-libretro"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="libretro gameboy core with up to 16 players support"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp DoubleCherryGB_libretro.so ${INSTALL}/usr/lib/libretro/
+}
