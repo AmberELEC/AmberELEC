20d83fc1d6801a77157642b5e8d940b005b04ca0
dhwz <dhwz@gmx.net>
2024-03-29 12:27:28 +0100

    bump SDL2_image, SDL2_mixer, SDL2_ttf

--- packages/tools/SDL2_mixer/package.mk
+++ packages/tools/SDL2_mixer/package.mk
@@ -2,14 +2,13 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="SDL2_mixer"
-PKG_VERSION="2.6.3"
-PKG_SHA256=""
+PKG_VERSION="2.8.0"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="${PKG_SITE}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 libogg libvorbis flac mpg123 libmodplug fluidsynth"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 libogg libvorbis flac mpg123 libmodplug libvorbisidec opusfile"
+PKG_LONGDESC="An audio mixer that supports various file formats for Simple Directmedia Layer. "
 PKG_DEPENDS_HOST="toolchain:host SDL2:host"
-PKG_LONGDESC="SDL2 mixer"
 
 pre_configure_host() {
   PKG_CMAKE_OPTS_HOST="-DSDL2MIXER_OPUS=OFF \
@@ -18,11 +17,20 @@ pre_configure_host() {
                        -DSDL2MIXER_FLAC=OFF \
                        -DSDL2MIXER_MIDI=OFF \
                        -DSDL2MIXER_VORBIS=OFF \
-                       -DSDL2MIXER_OGG=OFF"
+                       -DSDL2MIXER_OGG=OFF \
+                       -DSDL2MIXER_MOD_XMP=OFF \
+                       -DSDL2MIXER_WAVPACK=OFF"
 }
 
 pre_configure_target() {
-  SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config
-  PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET} --enable-music-mp3"
-  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_OPUS=OFF"
+PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_MIDI_FLUIDSYNTH=ON \
+                       -DSDL2MIXER_FLAC=ON \
+                       -DSDL2MIXER_MOD_MODPLUG=ON \
+                       -DSDL2MIXER_VORBIS_TREMOR=ON \
+                       -DSDL2MIXER_OGG=ON \
+                       -DSDL2MIXER_MP3=ON \
+                       -DSDL2MIXER_SAMPLES=OFF \
+                       -DSDL2MIXER_MOD_MODPLUG_SHARED=OFF \
+                       -DSDL2MIXER_MOD_XMP=OFF \
+                       -DSDL2MIXER_WAVPACK=OFF"
 }
