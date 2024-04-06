a007b3222679530f81d4e7be820a8769f3c9f38f
Tiago Medeiros <medeiros1990@yahoo.com>
2024-04-06 12:37:14 -0400

    Add fbgrab package

---
+++ packages/graphics/fbgrab/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
+
+PKG_NAME="fbgrab"
+PKG_VERSION="30aec96fb8de66095dee7203b5af54d0dd403c7f"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/GunnarMonell/fbgrab"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libpng"
+PKG_LONGDESC="FBGrab is a framebuffer screenshot program"
+
