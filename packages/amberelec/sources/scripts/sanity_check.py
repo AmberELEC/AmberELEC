2f31f69dfd9d9cfd1d9605e0a7a92ac8c95830b7
Tiago Medeiros <medeiros1990@yahoo.com>
2024-04-08 00:25:29 -0400

    Code cleanup

--- packages/amberelec/sources/scripts/sanity_check.py
+++ packages/amberelec/sources/scripts/sanity_check.py
@@ -11,6 +11,7 @@ def show_sanity_warn( message, force_quit=True ) :
 
 
 def sanity_log():
+	"""Run this when you want to show a generic error to the user and zip the logs"""
 	try :
 		command = ["zip", "-r", "/roms/logs.zip", "/tmp/logs"]
 		run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -18,6 +19,7 @@ def sanity_log():
 		show_sanity_warn( "A general error has occurred!\n\nIf you wish to get help with this issue,\nwe've zipped your log files.\n\nThey are located at <roms_folder>\\logs.zip\n\nMake sure to send us this file on discord\nWith as much information as you can\nIf you are going to need help!\n\nExiting...", False )
 
 def sanity_check(rom, platform, emulator, core, args):
+	"""Run this to check all parameters for known issues and warn the user"""
 
 	def search_zip_file(zip_file, filename):
 		"""Some roms come in .ZIP format, and sometimes they don't have the right ROM file we need, this checks for an extension and acts accordingly!"""
@@ -30,6 +32,7 @@ def sanity_check(rom, platform, emulator, core, args):
 			show_sanity_warn("The zip file you have attempted to open is either corrupt or not found\n\nExiting...")
 		return False
 
+	# Make sure the extension of the rom file can be easily read
 	extension = rom.suffix.lower()
 
 	# First we check duckstation. Our core does not support .pbp files
@@ -37,10 +40,9 @@ def sanity_check(rom, platform, emulator, core, args):
 		show_sanity_warn("The Duckstation core does not support .pbp files.\n\nPlease try another core for this rom!\n\nExiting...")
 
 	# Geolith is its own beast, but I need to double check if a zip file it opens has a .neo file inside...
-	if (core == "geolith"):
-		if (extension == ".zip"):
-			if( False == search_zip_file(args['rom'], ".neo") ):
-				show_sanity_warn("You tried to load a .zip file with the Geolith core\nbut it only supports either .neo files or\n.zip files with a .neo file inside which this one does not!\n\nExiting...")
+	if (core == "geolith" and extension == ".zip"):
+		if( False == search_zip_file(args['rom'], ".neo") ):
+			show_sanity_warn("You tried to load a .zip file with the Geolith core\nbut it only supports either .neo files or\n.zip files with a .neo file inside which this one does not!\n\nExiting...")
 
 	# I also need to make sure that if we're opening a .neo file, that we do it with Geolith!
 	if (extension == ".neo" and core != "geolith" ):
