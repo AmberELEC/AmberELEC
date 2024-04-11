0c6c3f3dfcb9910b86b4ce21c2c0d62beb602c7a
dhwz <dhwz@gmx.net>
2024-04-11 23:28:46 +0200

    bump xu10 kernel

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -5,7 +5,7 @@
 
 PKG_NAME="linux"
 if [[ "${DEVICE}" =~ RG351 ]]; then
-  PKG_VERSION="aacfe08251650f1ab1fb40c40bb9e91a52aebe30"
+  PKG_VERSION="aba4b6429c6599b05eb6febe1f547d7fb7eca5ed"
   PKG_URL="https://github.com/AmberELEC/kernel_rg351/archive/${PKG_VERSION}.tar.gz"
 elif [[ "${DEVICE}" =~ RG552 ]]; then
   PKG_VERSION="0c15ff851c1d24fac588bd4427bb45b9ab88f452"
