0c559f5f680c0b9d75e3bb049a0b14ec744f8a27
dhwz <dhwz@gmx.net>
2024-04-27 18:53:40 +0200

    enable retroachievements for nds

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="aed058586020eadac2016dfdfa1b7027e73ced35"
+PKG_VERSION="4b24815baade062da8692ff33c2dc3ac88dba9ac"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AmberELEC/emulationstation"
