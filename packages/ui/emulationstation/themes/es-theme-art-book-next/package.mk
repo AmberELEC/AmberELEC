a34e873c024cb2d73840adbdb504ac73155ea751
dhwz <dhwz@gmx.net>
2024-05-06 17:11:13 +0200

    bump default theme

--- packages/ui/emulationstation/themes/es-theme-art-book-next/package.mk
+++ packages/ui/emulationstation/themes/es-theme-art-book-next/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="284ab87135ba0fdae3d5e44e9d485a5f22ed10ec"
+PKG_VERSION="bae5e48e5b1ff4dd03853effd71a6b4c12deabe3"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/AmberELEC/art-book-next-amberelec"
