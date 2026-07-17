7256357c224da5e09a0178927646d71e344de82e
navy1978 <ivan.morelli@gmail.com>
2026-07-17 18:45:55 +0200

    Fix unavailable build package sources
    Update stale package sources, checksums, and pins for configtools, json-c, TIC-80, kernel firmware, WSDD2, and Waf.

--- packages/games/libretro/tic-80/package.mk
+++ packages/games/libretro/tic-80/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="tic-80"
-PKG_VERSION="ef023fb53992a73694d732a62a87861cd7ad8519"
+PKG_VERSION="9b79d496f0e3afcdf43bc5f97f59834114447b63" # 2026-05-05
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/nesbox/TIC-80"
 PKG_URL="${PKG_SITE}.git"
--- packages/python/devel/waf/package.mk
+++ packages/python/devel/waf/package.mk
@@ -3,13 +3,15 @@
 
 PKG_NAME="waf"
 PKG_VERSION="2.0.24"
-PKG_SHA256="6b78a3594540b232a154f64c4eb7e21f28e4e073c7e1605e4b9977519a6cb89e"
+PKG_SHA256="599ab1903b6f12f0683878d2cd4f73546e0ce26031efd05f3fbe5178e4ef8cda"
 PKG_LICENSE="MIT"
 PKG_SITE="https://waf.io"
-PKG_URL="https://waf.io/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://gitlab.com/ita1024/waf/-/archive/waf-${PKG_VERSION}/waf-waf-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_DIR="waf-waf-${PKG_VERSION}"
 PKG_LONGDESC="The Waf build system"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_host() {
+  (cd ${PKG_BUILD} && /usr/bin/python3 ./waf-light --make-waf --python=/usr/bin/python3 --interpreter='#!/usr/bin/python3')
   cp -pf ${PKG_BUILD}/waf ${TOOLCHAIN}/bin/
 }
