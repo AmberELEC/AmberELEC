88855b82a9f28e7ec7d7324def5e5c50a717c799
navy1978 <ivan.morelli@gmail.com>
2026-07-20 00:50:03 +0200

    Update RetroRun to 3.1.1

--- packages/games/tools/retrorun/package.mk
+++ packages/games/tools/retrorun/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present AmberELEC (https://github.com/AmberELEC)
 
 PKG_NAME="retrorun"
-PKG_VERSION="c4d80547a2fe7a2d065febb5a6cafc2413039547"
+PKG_VERSION="2ff54b025b72b8e7a035dbce283bef7d0134dd73"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun"
 PKG_URL="${PKG_SITE}.git"
